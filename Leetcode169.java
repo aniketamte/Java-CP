dcffa083b6463b9b932819e0fc6693449d34e2bb
aniketamte <aniketamte123@gmail.com>
2025-01-14 14:48:29 +0530

    Leetcode Question 169

---
+++ Leetcode169.java
@@ -0,0 +1,25 @@
+
+
+public class Leetcode169 {
+      //SOLUTION 1 => O(1)
+      public static int majorityElement(int nums[]){
+            int candidate = nums[0];
+            int count = 0;
+            for(int i: nums){
+                  if(i == candidate){
+                        count++;
+                  }else{
+                        count--;
+                        if(count == 0){
+                              candidate = i;
+                              count++;
+                        }
+                  }
+            }
+            return candidate;
+      }
+      public static void main(String[] args) {
+            int nums[] = {3,2,3};
+            System.out.println(majorityElement(nums));
+      }
+}
