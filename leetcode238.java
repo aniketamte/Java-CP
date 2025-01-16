0f332320e15338292f9328c9ec211ffeca86ec18
aniketamte <aniketamte123@gmail.com>
2025-01-16 16:17:15 +0530

    Leetcode Question 15

---
+++ leetcode238.java
@@ -0,0 +1,12 @@
+
+public class leetcode238 {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int ans[] = new int[nums.length];
+        int prod = 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
