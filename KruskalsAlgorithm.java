890f469069b7d832225a5d61f9c0e48902baffe3
aniketamte <aniketamte123@gmail.com>
2025-01-21 23:41:31 +0530

    Kruskals Algorithm

--- KruskalsAlgorithm.java
+++ KruskalsAlgorithm.java
@@ -1,27 +1,42 @@
+
 import java.util.ArrayList;
 
 public class KruskalsAlgorithm {
-      static class Edge implements Comparable<Edge>{
-            int src;
-            int dest;
-            int wt;
 
-            public Edge(int s, int d, int w){
-                  this.src = s;
-                  this.dest = d;
-                  this.wt = w;
-            }
-            @Override
-            public int compareTo(Edge e2){
-                  return this.wt - e2.wt;
-            }
-      }
-      static void createGraph(ArrayList<Edge> edges){
-            //edges
-            edges.add(new Edge(0, 1, 10));
-            edges.add(new Edge(0, 2, 15));
-            edges.add(new Edge(0, 3, 30));
-            edges.add(new Edge(1, 3, 40));
-            edges.add(new Edge(2, 3, 50));
-      }
+    static class Edge implements Comparable<Edge> {
+
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+
+        @Override
+        public int compareTo(Edge e2) {
+            return this.wt - e2.wt;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> edges) {
+        //edges
+        edges.add(new Edge(0, 1, 10));
+        edges.add(new Edge(0, 2, 15));
+        edges.add(new Edge(0, 3, 30));
+        edges.add(new Edge(1, 3, 40));
+        edges.add(new Edge(2, 3, 50));
+    }
+    static int n = 7;
+    static int par[] = new int[n];
+    static int rank[] = new int[n];
+
+    public static void init() {
+        //Start each element is parent for self
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+        }
+    }
 }
