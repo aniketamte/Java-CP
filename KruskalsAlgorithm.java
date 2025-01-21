e9bae990e5c036e5efd96ad22bbb0c2d64cbfc4a
aniketamte <aniketamte123@gmail.com>
2025-01-21 23:40:46 +0530

    Kruskals Algorithm

--- KruskalsAlgorithm.java
+++ KruskalsAlgorithm.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class KruskalsAlgorithm {
       static class Edge implements Comparable<Edge>{
             int src;
@@ -13,5 +15,13 @@ public class KruskalsAlgorithm {
             public int compareTo(Edge e2){
                   return this.wt - e2.wt;
             }
+      }
+      static void createGraph(ArrayList<Edge> edges){
+            //edges
+            edges.add(new Edge(0, 1, 10));
+            edges.add(new Edge(0, 2, 15));
+            edges.add(new Edge(0, 3, 30));
+            edges.add(new Edge(1, 3, 40));
+            edges.add(new Edge(2, 3, 50));
       }
 }
