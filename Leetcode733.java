327cb833da11193aa5ce2f2af974d3e6659b3f94
aniketamte <aniketamte123@gmail.com>
2025-01-22 18:02:17 +0530

    Leetcode Question 733

--- Leetcode733.java
+++ Leetcode733.java
@@ -1,7 +1,14 @@
 
 public class Leetcode733 {
       public int[][] helper(int[][] image, int sr, int sc, int color, boolean vis[][], int orgCol) {
-
+            //Left
+            helper(image, sr, sc-1, color, vis, orgCol);
+            //Right
+            helper(image, sr, sc+1, color, vis, orgCol);
+            //Up
+            helper(image, sr-1, sc, color, vis, orgCol);
+            //Down
+            helper(image, sr+1, sc, color, vis, orgCol);
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
