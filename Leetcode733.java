427af56ebf36d39a7ea98941b667b2e0c34da3fa
aniketamte <aniketamte123@gmail.com>
2025-01-22 18:00:31 +0530

    Leetcode Question 733

--- Leetcode733.java
+++ Leetcode733.java
@@ -1,5 +1,16 @@
-public class Leetcode733{
-      public static void main(String[] args) {
-            
-      }
+
+public class Leetcode733 {
+      public int[][] helper(int[][] image, int sr, int sc, int color, boolean vis[][], int orgCol) {
+
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+      boolean vis[][] = new boolean[image.length][image[0].length];
+      helper(image, sr, sc, color, vis, image[sr][sc]);
+      return image;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
