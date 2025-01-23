23f472df413474957b073a3ccc4b2cc888bc2561
aniketamte <aniketamte123@gmail.com>
2025-01-23 19:12:01 +0530

    0-1 Knapsack problem

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -1,4 +1,7 @@
 public class KnapsackProblem01 {
+      public static int knapsanck(int val[], int wt[], int W, int n){
+            
+      }
       public static void main(String[] args) {
         int val[] = {15, 14, 10, 45, 30};
         int wt[] = {2, 5, 1, 3, 4};
