91d988e99eb4cda227fadf88af26e509b37a7ccf
aniketamte <aniketamte123@gmail.com>
2025-01-23 19:14:01 +0530

    0-1 kNAPSACK pROBLEM

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -1,23 +1,26 @@
+
 public class KnapsackProblem01 {
-      public static int knapsanck(int val[], int wt[], int W, int n){
-            //W = Capacity of Bag  
-          if (W == 0 || n == 0) {
-              return 0;
-          }
-          if (wt[n - 1] <= W) {  //Valid condition
-              //include
 
-              int ans1 = val[n - 1] + knapsanck(val, wt, W - wt[n - 1], n - 1);
+    public static int knapsanck(int val[], int wt[], int W, int n) {
+        //W = Capacity of Bag  
+        if (W == 0 || n == 0) {
+            return 0;
+        }
+        if (wt[n - 1] <= W) {  //Valid condition
+            //include
 
-              //exlude
-              int ans2 = knapsanck(val, wt, W, n - 1);
-              return Math.max(ans1, ans2);
-          } else {
-              //Not valid Condition
-              return knapsanck(val, wt, W, n - 1);
-          }
-      }
-      public static void main(String[] args) {
+            int ans1 = val[n - 1] + knapsanck(val, wt, W - wt[n - 1], n - 1);
+
+            //exlude
+            int ans2 = knapsanck(val, wt, W, n - 1);
+            return Math.max(ans1, ans2);
+        } else {
+            //Not valid Condition
+            return knapsanck(val, wt, W, n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
         int val[] = {15, 14, 10, 45, 30};
         int wt[] = {2, 5, 1, 3, 4};
         int W = 7;
