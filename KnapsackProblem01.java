b0ddc7b69c07815354935f230a395859a7a6f82e
aniketamte <aniketamte123@gmail.com>
2025-01-24 22:38:11 +0530

    01 Knapsack Problem

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -47,45 +47,46 @@ public class KnapsackProblem01 {
     }
 
     //Tabular Method Code for 0-1 Knapsack
-     public static int knapsackTab(int val[], int wt[], int W){
-         int n = val.length;
-         int dp[][] = new int[n + 1][W + 1];
-         for (int i = 0; i < dp.length; i++) {
-             //for 0th column
-             dp[i][0] = 0;
-         }
-         for (int j = 0; j < dp[0].length; j++) {
-             //for 0th row
-             dp[0][j] = 0;
-         }
-         for (int i = 1; i < n + 1; i++) {
-             for (int j = 1; j < W + 1; j++) {
-                 int v = val[i - 1]; //ith item val
-                 int w = wt[i - 1];  //ith item wait
-                 if (w <= j) {
-                     //for valid condition
-                     int incProfit = v + dp[i - 1][j - w];
-                     int excProfit = dp[i - 1][j];
-                     dp[i][j] = Math.max(incProfit, excProfit);
-                 } else {
-                     //invalid condition
-                     int excProfit = dp[i - 1][j];
-                     dp[i][j] = excProfit;
-                 }
-             }
-         }
-         print(dp);
-         return dp[n][W];
-     }
-     public static void print(int dp[][]){
-         for (int i = 0; i < dp.length; i++) {
-             for (int j = 0; j < dp[0].length; j++) {
-                 System.out.print(dp[i][j] + " ");
-             }
-             System.out.println();
-         }
-         System.out.println();
-     }
+    public static int knapsackTab(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            //for 0th column
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            //for 0th row
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i - 1]; //ith item val
+                int w = wt[i - 1];  //ith item wait
+                if (w <= j) {
+                    //for valid condition
+                    int incProfit = v + dp[i - 1][j - w];
+                    int excProfit = dp[i - 1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                } else {
+                    //invalid condition
+                    int excProfit = dp[i - 1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        print(dp);
+        return dp[n][W];
+    }
+
+    public static void print(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         int val[] = {15, 14, 10, 45, 30};
