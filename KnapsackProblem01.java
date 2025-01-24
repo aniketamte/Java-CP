74393c43af4c6dbddc8091468990fc9153984a5c
aniketamte <aniketamte123@gmail.com>
2025-01-24 22:37:10 +0530

    01 Knapsack Problem

--- KnapsackProblem01.java
+++ KnapsackProblem01.java
@@ -74,6 +74,8 @@ public class KnapsackProblem01 {
                  }
              }
          }
+         print(dp);
+         return dp[n][W];
      }
 
     public static void main(String[] args) {
