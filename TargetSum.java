f3d9fced7a52d7a1150a7c7cab4933f072b24f68
aniketamte <aniketamte123@gmail.com>
2025-01-25 12:00:50 +0530

    Target Subset Sum Question Using DP

--- TargetSum.java
+++ TargetSum.java
@@ -2,9 +2,15 @@ public class TargetSum {
       //Time Complexity ---> O(n * sum)
 
     public static boolean targetSumSubset(int arr[], int sum){
+        int n = arr.length;
+        boolean dp[][] = new boolean[n + 1][sum + 1];
 
+        //intialization for 0th column
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
     }
-    
+
       public static void print(boolean dp[][]) {
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
