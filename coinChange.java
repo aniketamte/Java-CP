9ee7a02ae86570a8d2e7946afebb10fd4e491017
aniketamte <aniketamte123@gmail.com>
2025-01-25 22:16:23 +0530

    Coin change using DP

--- coinChange.java
+++ coinChange.java
@@ -10,6 +10,18 @@ public class coinChange{
           for (int j = 1; j < sum + 1; j++) {
               dp[0][j] = 0;   //Not Possible Solution
           }
+          for (int i = 1; i < n + 1; i++) {
+              for (int j = 1; j < sum + 1; j++) {
+                  if (coins[i - 1] <= j) {
+                      //valid
+                      dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                  } else {
+                      //invalid
+                      dp[i][j] = dp[i - 1][j];
+                  }
+              }
+          }
+          return dp[n][sum];
       }
       public static void main(String[] args) {
         int coins[] = {1, 2, 3};
