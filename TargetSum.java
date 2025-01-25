b97ef9e9d07cc8269edf248d59a6feab98214f73
aniketamte <aniketamte123@gmail.com>
2025-01-25 11:59:54 +0530

    Target Subset Sum Question Using DP

--- TargetSum.java
+++ TargetSum.java
@@ -1,4 +1,13 @@
 public class TargetSum {
+      public static void print(boolean dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
       public static void main(String[] args) {
         int arr[] = {4, 2, 7, 1, 3};
         int sum = 10;
