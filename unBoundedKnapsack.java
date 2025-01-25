d03ce473a4f7c887e18c087a792d65717e8f0b09
aniketamte <aniketamte123@gmail.com>
2025-01-25 12:31:55 +0530

    UnBounded knapsack problem

---
+++ unBoundedKnapsack.java
@@ -0,0 +1,8 @@
+public class unBoundedKnapsack {
+      public static void main(String[] args) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+        System.out.println(unboundedKnap(val, wt, W));
+    }
+}
