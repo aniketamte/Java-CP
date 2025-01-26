7c0faf0e6726709684f59efb12d17c11fe9bdb30
aniketamte <aniketamte123@gmail.com>
2025-01-26 08:34:02 +0530

    Weekly Contest 434 Leetcode

---
+++ contest1.java
@@ -0,0 +1,10 @@
+public class contest1 {
+      //weekly contest 434 ====> Q1
+      //Count Partitions with Even Sum Difference
+      public static int countEvenSumDifferencePartitions(int[] nums) {
+
+      }
+      public static void main(String[] args) {
+            
+      }
+}
