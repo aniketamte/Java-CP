3f723deb5285c6d0e2d7305021fc477f60ea7b46
aniketamte <aniketamte123@gmail.com>
2025-01-26 08:39:06 +0530

    Leetcode Weekly Contest 434 ---> Question2

--- contest434Q2.java
+++ contest434Q2.java
@@ -1,8 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class contest434Q2 {
       public class UserMentions{
-            
+
       }
       public static void main(String[] args){
+            // Example 1
+        int numberOfUsers1 = 2;
+        List<List<String>> events1 = Arrays.asList(
+            Arrays.asList("MESSAGE", "10", "id1 id0"),
+            Arrays.asList("OFFLINE", "11", "0"),
+            Arrays.asList("MESSAGE", "71", "HERE")
+        );
+        System.out.println(Arrays.toString(calculateMentions(numberOfUsers1, events1))); // Output: [2, 2]
 
+        // Example 2
+        int numberOfUsers2 = 2;
+        List<List<String>> events2 = Arrays.asList(
+            Arrays.asList("MESSAGE", "10", "id1 id0"),
+            Arrays.asList("OFFLINE", "11", "0"),
+            Arrays.asList("MESSAGE", "12", "ALL")
+        );
+        System.out.println(Arrays.toString(calculateMentions(numberOfUsers2, events2))); // Output: [2, 2]
+
+        // Example 3
+        int numberOfUsers3 = 2;
+        List<List<String>> events3 = Arrays.asList(
+            Arrays.asList("OFFLINE", "10", "0"),
+            Arrays.asList("MESSAGE", "12", "HERE")
+        );
+        System.out.println(Arrays.toString(calculateMentions(numberOfUsers3, events3))); // Output: [0, 1]
       }
 }
