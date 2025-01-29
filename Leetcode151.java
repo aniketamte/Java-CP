7d122da714eba80e506c93a0cad1e3e4fdb521e2
aniketamte <aniketamte123@gmail.com>
2025-01-29 23:24:14 +0530

    Leetcode Question 151 ----> Reverse words in String

---
+++ Leetcode151.java
@@ -0,0 +1,18 @@
+
+public class Leetcode151 {
+
+    public static String reverseWords(String s) {
+      String[] words = s.split(" +");
+      StringBuilder sb= new StringBuilder();
+      for(int i=words.length-1; i>=0; i--){
+            sb.append(words[i]);
+            sb.append(" ");
+      }
+      return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+      String s = "the sky is blue";
+      System.out.println(reverseWords(s));
+    }
+}
