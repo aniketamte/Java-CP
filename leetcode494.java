031aee0f8e5b7a75c6ad2c586e95afac55c12bf7
aniketamte <aniketamte123@gmail.com>
2025-01-29 18:21:47 +0530

    Leetcode 494 ---> Target Sum

--- leetcode494.java
+++ leetcode494.java
@@ -1,6 +1,8 @@
+
 public class leetcode494 {
-      //Leetcode 494 ----> Target Sum
-      public static int findTargetSumWays(int[] nums, int target) {
+    //Leetcode 494 ----> Target Sum
+
+    public static int findTargetSumWays(int[] nums, int target) {
         return helper(nums, target, 0, 0);
     }
 
@@ -14,8 +16,10 @@ public class leetcode494 {
 
         return subtract + add;
     }
-      public static void main(String[] args) {
-            int nums[] = {1,1,1,1,1};
-            int target=3;
-      }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 1, 1, 1, 1};
+        int target = 3;
+        System.out.println(findTargetSumWays(nums, target));
+    }
 }
