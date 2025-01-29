1ea16de33c44039c95cc505b8b750d653046af2d
aniketamte <aniketamte123@gmail.com>
2025-01-29 10:51:28 +0530

    Day 21 --->Sort 0s, 1s and 2s

---
+++ GFG21.java
@@ -0,0 +1,7 @@
+public class GFG21 {
+      //GfG 160 Day Challenge ----> Day 21
+    //Day 21 --->Sort 0s, 1s and 2s
+    public static void main(String[] args) {
+      int arr[] = {0, 1, 2, 0, 1, 2};
+    }
+}
