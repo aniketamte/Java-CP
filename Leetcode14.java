11d3abee864c86ca145049b37a7eeeaabd9d2212
aniketamte <aniketamte123@gmail.com>
2025-02-01 11:41:54 +0530

    Leetcode : 14 -->Longest Common Prefix

---
+++ Leetcode14.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+
+public class Leetcode14 {
+
+    public static String longestCommonPrefix(String[] strs) {
+        //First sort the array
+        Arrays.sort(strs);
+        //First String
+        String str1 = strs[0];
+        //Last String 
+        String str2 = strs[strs.length - 1];
+
+        //comapre index by index
+        int index = 0;
+        while (index < str1.length()) {
+            if (str1.charAt(index) == str2.charAt(index)) {
+                index++;
+            } else {
+                //if doesn't match
+                break;
+            }
+        }
+        return index == 0 ? "" : str1.substring(0, index);
+    }
+
+    public static void main(String[] args) {
+        String strs[] = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
