e3a449817f99972d67ef6d85634dfd56bc3c945e
aniketamte <aniketamte123@gmail.com>
2025-02-02 09:45:30 +0530

    Leetcode : 3442 ----> Maximum Difference Between Even and Odd Frequency I

--- Leetcode3442.java
+++ Leetcode3442.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 public class Leetcode3442 {
 
+      //Leetcode : 3442 --> Maximum Difference Between Even and Odd Frequency I
+
     public static int maxDifference(String s) {
         Map<Character, Integer> freqMap = new HashMap<>();
 
