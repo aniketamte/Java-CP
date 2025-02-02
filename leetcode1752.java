049a695d8d941e97ccd01e08f6a082f3e0dece1f
aniketamte <aniketamte123@gmail.com>
2025-02-02 09:39:37 +0530

    Leetcode : 1752 ---> Check if Array Is Sorted and Rotated

---
+++ leetcode1752.java
@@ -0,0 +1,24 @@
+
+public class leetcode1752 {
+
+    public static boolean check(int[] nums) {
+        int spike = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                spike++;
+            }
+        }
+        if (nums[nums.length - 1] > nums[0]) {
+            spike++;
+        }
+        if (spike > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {3, 4, 5, 1, 2};
+        System.out.println(check(nums));
+    }
+}
