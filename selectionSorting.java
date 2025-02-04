7f863357fbb86894a480e421e6950e4c2fdf0c51
aniketamte <aniketamte123@gmail.com>
2025-02-04 17:19:26 +0530

    Selection Sort Technique

--- selectionSorting.java
+++ selectionSorting.java
@@ -1,7 +1,22 @@
 
 public class selectionSorting {
 
-    public static void SelectionSort(int arr[]) {
+    public static void SelectionSortInc(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void SelectionSortDec(int arr[]) {
         for (int i = 0; i < arr.length - 1; i++) {
             int minPos = i;
             for (int j = i + 1; j < arr.length; j++) {
@@ -27,7 +42,9 @@ public class selectionSorting {
 
     public static void main(String[] args) {
         int arr[] = {5, 4, 1, 3, 2};
-        SelectionSort(arr);
+        SelectionSortInc(arr);
+        printArr(arr);
+        SelectionSortDec(arr);
         printArr(arr);
     }
 }
