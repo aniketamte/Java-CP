f08883f7a757171857885f516120c37f3c9a424e
aniketamte <aniketamte123@gmail.com>
2025-02-04 17:55:21 +0530

    Inbuilt Sort

--- inBuiltSort.java
+++ inBuiltSort.java
@@ -1,20 +1,24 @@
 
-import java.util.Arrays;
+import java.util.*;
 
 public class inBuiltSort {
       //Print Array
-      public static void printArr(int arr[]){
-            for(int i=0; i<arr.length; i++){
+      public static void printArr(Integer arr[]){
+            for(Integer i=0; i<arr.length; i++){
                   System.out.print(arr[i] + " ");
             }
             System.out.println();
       }
       public static void main(String[] args) {
-            int arr[] = {5,4,1,3,2};
+            Integer arr[] = {5,4,1,3,2};
             /*Arrays.sort(arr);
             printArr(arr);*/
 
-            Arrays.sort(arr, 0, 3);
-            printArr(arr); 
+            /*Arrays.sort(arr, 0, 3);
+            printArr(arr); */
+
+            //print in reverse order
+            Arrays.sort(arr, Collections.reverseOrder());
+            printArr(arr);
       }
 }
