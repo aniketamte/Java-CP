7156b423dc7c69c76614b4d1a192d104b9d0da5c
aniketamte <aniketamte123@gmail.com>
2025-02-04 17:37:06 +0530

    Insertion Sort Technique

---
+++ insertionSort.java
@@ -0,0 +1,30 @@
+
+public class insertionSort {
+
+    public static void InsertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int previous = i - 1;
+
+            while (previous >= 0 && arr[previous] > current) {
+                arr[previous + 1] = arr[previous];
+                previous--;
+            }
+            arr[previous + 1] = current;
+        }
+    }
+
+    //print arr 
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        InsertionSort(arr);
+        printArr(arr);
+    }
+}
