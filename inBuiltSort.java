242ec0bf503b56ecc9a5cb6d5404ec8d6e94e7e1
aniketamte <aniketamte123@gmail.com>
2025-02-04 17:50:53 +0530

    Inbuilt Sort

--- inBuiltSort.java
+++ inBuiltSort.java
@@ -11,8 +11,8 @@ public class inBuiltSort {
       }
       public static void main(String[] args) {
             int arr[] = {5,4,1,3,2};
-            Arrays.sort(arr);
-            printArr(arr);
+            /*Arrays.sort(arr);
+            printArr(arr);*/
 
             Arrays.sort(arr, 0, 3);
             printArr(arr); 
