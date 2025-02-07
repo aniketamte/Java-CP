5960464dc05e0e8ee5ddffa48aabb4c692780889
aniketamte <aniketamte123@gmail.com>
2025-02-07 18:11:55 +0530

    Leetcode : 3160 ---> Find the Number of Distinct Colors Among the Balls

--- Leetcode3160.java
+++ Leetcode3160.java
@@ -5,6 +5,8 @@ public class Leetcode3160 {
 
       //Find the Number of Distinct Colors Among the Balls
 
+      //POTD ---> 07/02/2025
+
     public static int[] queryResults(int limit, int[][] queries) {
         Map<Integer, Integer> node = new HashMap<>();
         Map<Integer, Integer> color = new HashMap<>();
