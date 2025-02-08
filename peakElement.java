bf8159ba1dd496a10cc95c42833bcae08dbab3cb
aniketamte <aniketamte123@gmail.com>
2025-02-08 15:10:34 +0530

    Find a peak element

---
+++ peakElement.java
@@ -0,0 +1,20 @@
+public class peakElement{
+      public static int solve(int[] A) {
+        int l = 0;
+        int r = A.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (A[mid] > A[mid + 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return A[l];
+    }
+    public static void main(String[] args) {
+      int A[] = {5, 17, 100, 11};
+      System.out.println(solve(A));
+    }
+}
