40008c1391fdba9319edd37857a6dac7d3db6c6f
aniketamte <aniketamte123@gmail.com>
2025-03-04 16:19:51 +0530

    Leetcode : 237 ====> Delete Node in a Linked List

--- Leetcode237.java
+++ Leetcode237.java
@@ -15,5 +15,21 @@ public class Leetcode237 {
         node.val = node.next.val;
         node.next = node.next.next;
     }
-    
+
+    public static void main(String[] args) {
+        // Creating the linked list: 4 -> 5 -> 1 -> 9
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        // Deleting node with value 5
+        deleteNode(head.next);
+
+        System.out.println("List after deleting node 5:");
+        printList(head);
+    }
 }
