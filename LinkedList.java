f06336375d263a9c351adfe9c301b7e00fc9e985
aniketamte <aniketamte123@gmail.com>
2025-03-08 17:48:34 +0530

    Revising LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,22 +1,42 @@
+
 public class LinkedList {
-      static class Node{
-            int data;
-            Node next;
 
-            //Constructor
+    static class Node {
 
-            Node(int data){
-                  this.data=data;
-                  this.next=null;
-            }
-      }
+        int data;
+        Node next;
 
-      public static Node head;
-      public static Node tail;
+        //Constructor
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
-      public static void main(String[] args) {
-            LinkedList ll = new LinkedList();
-            ll.head = new Node(1);
-            ll.head.next = new Node(2);
-      }
+    public static Node head;
+    public static Node tail;
+
+    //Add in Linked List
+    public static void addFirst(int data) {
+        //create new node
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        //newnode next ---> head
+        newNode.next = head;
+        //head--> newNode
+        head = newNode;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        // ll.head = new Node(1);
+        // ll.head.next = new Node(2);
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+    }
 }
