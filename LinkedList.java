35afabc2081bd46dc18c7d3c3dbfac718837f576
aniketamte <aniketamte123@gmail.com>
2025-03-08 17:35:24 +0530

    Revising LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,16 @@
+public class LinkedList {
+      static class Node{
+            int data;
+            Node next;
+
+            //Constructor
+
+            Node(int data){
+                  this.data=data;
+                  this.next=null;
+            }
+      }
+
+      public static Node head;
+      public static Node tail;
+}
