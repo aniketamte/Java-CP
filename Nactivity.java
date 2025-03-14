9f9d5a036f1e9b7cc9d4d0b71298550577fd29da
aniketamte <aniketamte123@gmail.com>
2025-03-14 19:59:17 +0530

    N-activity problem : Greedy ALgorithm

--- Nactivity.java
+++ Nactivity.java
@@ -1,11 +1,20 @@
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Nactivity {
 
     public static void main(String[] args) {
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[] = {2, 4, 6, 7, 9, 9};
+
+        //Sorting
+        int activities[][] = new int[start.length][3];
+
+        for(int i=0; i<start.length; i++){
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
 
         //end time basis sorted
         int maxAct = 0;
