0c19ca10959ca152360bbf19bb99f9941ee86422
aniketamte <aniketamte123@gmail.com>
2025-03-14 23:53:40 +0530

    Nactivity Problem ===> Greedy Algorithm

--- Nactivity.java
+++ Nactivity.java
@@ -2,10 +2,9 @@
 import java.util.*;
 
 public class Nactivity {
-    
+
     //N_Activity Problem Using Greedy Algorithm
     //14-03-2025
-
     public static void main(String[] args) {
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[] = {2, 4, 6, 7, 9, 9};
@@ -13,7 +12,7 @@ public class Nactivity {
         //Sorting
         int activities[][] = new int[start.length][3];
 
-        for(int i=0; i<start.length; i++){
+        for (int i = 0; i < start.length; i++) {
             activities[i][0] = i;
             activities[i][1] = start[i];
             activities[i][2] = end[i];
@@ -32,18 +31,18 @@ public class Nactivity {
         ans.add(0);
         int lastEnd = activities[0][2];
 
-        for(int i=0; i<end.length; i++){
-            if(activities[i][1] >= lastEnd){
-                  //activity select
-                  maxAct++;
-                  ans.add(activities[i][0]);
-                  lastEnd = activities[i][2];
+        for (int i = 0; i < end.length; i++) {
+            if (activities[i][1] >= lastEnd) {
+                //activity select
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
 
         System.out.println("Max Activities : " + maxAct);
 
-        for(int i=0; i<ans.size(); i++){
+        for (int i = 0; i < ans.size(); i++) {
             System.out.println("A" + ans.get(i) + " ");
         }
         System.out.println();
