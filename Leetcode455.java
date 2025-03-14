a4f6c0b4592728ba9d183befb483e393b56b2805
aniketamte <aniketamte123@gmail.com>
2025-03-14 18:27:57 +0530

    Leetcode 455====> Assign Cookies

--- Leetcode455.java
+++ Leetcode455.java
@@ -3,18 +3,36 @@ import java.util.Arrays;
 public class Leetcode455 {
 
     //Leetcode 168
+    // public static int findContentChildren(int[] g, int[] s) {
+    //     Arrays.sort(g);
+    //     Arrays.sort(s);
+    //     int contentChildren = 0;
+    //     int cookieIndex = 0;
+    //     while (cookieIndex < s.length && contentChildren < g.length) {
+    //         if (s[cookieIndex] >= g[contentChildren]) {
+    //             contentChildren++;
+    //         }
+    //         cookieIndex++;
+    //     }
+    //     return contentChildren;
+    // }
+
     public static int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int contentChildren = 0;
-        int cookieIndex = 0;
-        while (cookieIndex < s.length && contentChildren < g.length) {
-            if (s[cookieIndex] >= g[contentChildren]) {
-                contentChildren++;
+        int j = 0, ans = 0;
+        for (int i : g) {
+            while (j < s.length && s[j] < i) {
+                j++;
             }
-            cookieIndex++;
+            if (j == s.length) {
+                return ans; 
+            }else {
+                j++;
+                ans++;
+            }
         }
-        return contentChildren;
+        return ans;
     }
 
     public static void main(String[] args) {
