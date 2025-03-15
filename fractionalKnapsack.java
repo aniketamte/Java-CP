46b8be060dd81775d22a270f7a62faca04fa33fc
aniketamte <aniketamte123@gmail.com>
2025-03-15 14:40:30 +0530

    Fractional Knapsack problem ====> Greedy Algorithm

--- fractionalKnapsack.java
+++ fractionalKnapsack.java
@@ -2,6 +2,11 @@ public class fractionalKnapsack {
       //Fractional Knapsack problem ====> Greedy Algorithm
       //15-03-2025
       public static void main(String[] args) {
-            
+            int val[] = {60, 100, 120};
+            int weight[]= {10, 20, 30};
+            int W = 50;
+
+            double ratio[][] = new double[val.length][2];
+            //0th col ==>idx; 1st col ==> ratio
       }
 }
