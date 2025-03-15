4aca32cd637e4ced3323af2af2d882ea4dc791d5
aniketamte <aniketamte123@gmail.com>
2025-03-15 15:06:14 +0530

    Minimum Absolute Difference Pair ===> Greedy Algorithm

--- AbsoluteDifferencePair.java
+++ AbsoluteDifferencePair.java
@@ -1,7 +1,15 @@
+import java.util.Arrays;
+
 public class AbsoluteDifferencePair {
-      //Minimum Absolute Difference Pair
+      //Minimum Absolute Difference Pair ===> Greedy Algorithm
       //15-03-2025
       public static void main(String[] args) {
-            
+            int A[] = {1,2,3};
+            int B[] = {2,1,3};
+
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            int minDiff = 0;
       }
 }
