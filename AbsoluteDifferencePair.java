7b09dc2ffb929175568d45251d897b0d72ace9bf
aniketamte <aniketamte123@gmail.com>
2025-03-15 15:09:54 +0530

    Minimum Absolute Difference Pair ===> Greedy Algorithm

--- AbsoluteDifferencePair.java
+++ AbsoluteDifferencePair.java
@@ -6,8 +6,11 @@ public class AbsoluteDifferencePair {
     //15-03-2025
 
     public static void main(String[] args) {
-        int A[] = {1, 2, 3};
-        int B[] = {2, 1, 3};
+      //   int A[] = {1, 2, 3};
+      //   int B[] = {2, 1, 3};
+
+        int A[] = {4,1,8,7};
+        int B[] = {2,3,6,5};
 
         Arrays.sort(A);
         Arrays.sort(B);
