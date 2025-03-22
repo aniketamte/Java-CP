2fc4d712fb63b8f7e0bfb009e1d4121b92d60a3a
aniketamte <aniketamte123@gmail.com>
2025-03-22 13:36:59 +0530

    Leetcode : 1143 ===> Longest Common Subsequence

--- Leetcode1143.java
+++ Leetcode1143.java
@@ -15,6 +15,24 @@ public class Leetcode1143{
                   return Math.max(ans1, ans2);
             }
       }
+
+
+      // Solve using Memoization code Method
+
+      public static int LCS2(String str1, String str2, int n, int m) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) {   //Same
+            return LCS2(str1, str2, n - 1, m - 1) + 1;
+        } else {
+            //Different
+            int ans1 = LCS2(str1, str2, n - 1, m);
+            int ans2 = LCS2(str1, str2, n, m - 1);
+            return Math.max(ans1, ans2);
+        }
+    }
       public static void main(String[] args) {
             String str1 = "abcdge";
             String str2 = "abedg";
