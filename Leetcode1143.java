473f60cdb037eb37738db75574d4f74c87b0530f
aniketamte <aniketamte123@gmail.com>
2025-03-22 14:11:19 +0530

    Leetcode : 1143 ===> Longest Common Subsequence

--- Leetcode1143.java
+++ Leetcode1143.java
@@ -39,38 +39,47 @@ public class Leetcode1143 {
 
     // Solve Using Tabulation Method
     public static int longestCommonSubsequence(String text1, String text2) {
-      int n = text1.length();
-      int m = text2.length();
-      
-      int dp[][] = new int[n+1][m+1];
+        int n = text1.length();
+        int m = text2.length();
 
-      //Optional Initialization ---> (There is no need to to write the below loop)
+        int dp[][] = new int[n + 1][m + 1];
 
-      for(int i=0; i<n+1; i++){
-            for(int j=0; j<m+1; j++){
-                  if(i == 0 || j == 0){
-                        dp[i][j] = 0;
-                  }
+        //Optional Initialization ---> (There is no need to to write the below loop)
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
             }
-      }
+        }
 
-      //Strat with tabulation code
-      for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                  if(text1.charAt(i-1) == text2.charAt(j-1)){
-                        //Same
-                        dp[i][j] = dp[i-1][j-1] + 1;
-                  } else{
-                        //different
-                        int ans1 = dp[i-1][j];
-                        int ans2 = dp[i][j-1];
-                        dp[i][j] = Math.max(ans1, ans2);
-                  }
+        //Strat with tabulation code
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    //Same
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    //different
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
             }
-      }
-      return dp[n][m];
+        }
+        print(dp);
+        return dp[n][m];
     }
 
+    public static void print(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         String str1 = "abcdge";
