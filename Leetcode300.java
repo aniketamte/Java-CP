fd17568150cdf8568c278b3ffde22b0c2a6e4ede
aniketamte <aniketamte123@gmail.com>
2025-03-22 19:33:01 +0530

    Leetcode : 300 ---> Longest Increasinng Subsequence

--- Leetcode300.java
+++ Leetcode300.java
@@ -54,6 +54,16 @@ public class Leetcode300 {
         return LCS(nums, nums2);
     }
 
+    public static void print(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int nums[] = {50, 3, 10, 7, 40, 80};
         System.out.println(lengthOfLIS(nums));
