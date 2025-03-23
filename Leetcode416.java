664b300ac7034c8950bfe19e501cfb5d20312ba4
aniketamte <aniketamte123@gmail.com>
2025-03-23 14:38:52 +0530

    Leetcode : 416 ===> Partition Equal Subset Sum

--- Leetcode416.java
+++ Leetcode416.java
@@ -3,9 +3,8 @@ import java.util.Arrays;
 
 public class Leetcode416 {
 
-      //Leetcode : 416 ===> Partition Equal Subset Sum
-      //23-03-2025
-
+    //Leetcode : 416 ===> Partition Equal Subset Sum
+    //23-03-2025
     private static boolean memo(int[] nums, int[][] dp, int sum, int idx) {
         if (sum == 0) {
             return true;
