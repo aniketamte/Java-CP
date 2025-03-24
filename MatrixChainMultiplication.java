f241a0d67bb60d944e1b83c089e681ecba9f9726
aniketamte <aniketamte123@gmail.com>
2025-03-24 18:09:29 +0530

    Matrix chain Multiplication ===> GFG Practice Question

--- MatrixChainMultiplication.java
+++ MatrixChainMultiplication.java
@@ -1,35 +1,24 @@
+
 import java.util.Arrays;
 
-public class MatrixChainMultiplication{
+public class MatrixChainMultiplication {
 
-      //Matrix chain Multiplication ===> GFG Practice Question
-      //Solve this question using dynamic programming
-      //22-03-2025
-
-      //solve using Recursion Method
-      public static int MCM(int arr[], int i, int j){
-            //i ===> Strating Point
-            //j ===> Ending Point
-
-            if(i==j){
-                  return 0;  //single matrix case
+    //Matrix chain Multiplication ===> GFG Practice Question
+    //Solve this question using dynamic programming
+    //22-03-2025
+    //Print table
+    public static void print(int dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
             }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
-            int ans = Integer.MAX_VALUE;
-
-            for(int k=i; k<=j-1; k++){
-                  int cost1 = MCM(arr, i, k);  //arr[i-1] * arr[k]
-                  int cost2 = MCM(arr, k+1, j);  //arr[k] * arr[j]
-                  int cost3 = arr[i-1] * arr[k] * arr[j];
-                  int finalCost = cost1 + cost2 + cost3;
-                  ans = Math.min(ans, finalCost);
-            }
-            return ans;
-      }
-
-      //Solve using Memoization Method
-
-      public static int MCM2(int arr[], int i, int j, int dp[][]) {
+    //solve using Recursion Method
+    public static int MCM(int arr[], int i, int j) {
         //i ===> Strating Point
         //j ===> Ending Point
 
@@ -37,7 +26,28 @@ public class MatrixChainMultiplication{
             return 0;  //single matrix case
         }
 
-        if(dp[i][j] != -1){  //answer is already calculated
+        int ans = Integer.MAX_VALUE;
+
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = MCM(arr, i, k);  //arr[i-1] * arr[k]
+            int cost2 = MCM(arr, k + 1, j);  //arr[k] * arr[j]
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+        return ans;
+    }
+
+    //Solve using Memoization Method
+    public static int MCM2(int arr[], int i, int j, int dp[][]) {
+        //i ===> Strating Point
+        //j ===> Ending Point
+
+        if (i == j) {
+            return 0;  //single matrix case
+        }
+
+        if (dp[i][j] != -1) {  //answer is already calculated
             return dp[i][j];
         }
 
@@ -53,19 +63,18 @@ public class MatrixChainMultiplication{
         return dp[i][j] = ans;
     }
 
-      
-      public static void main(String[] args) {
-            int arr[] = {1,2,3,4,3};  //n=5
-            int n = arr.length;
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 3};  //n=5
+        int n = arr.length;
 
-            int dp[][] = new int[n][n];
+        int dp[][] = new int[n][n];
 
-            for(int i=0; i<n; i++){   //fill all index in table as -1
-                  Arrays.fill(dp[i], -1);
-            }
+        for (int i = 0; i < n; i++) {   //fill all index in table as -1
+            Arrays.fill(dp[i], -1);
+        }
 
-            System.out.println("Solve using Recursion Method : " + MCM(arr, 1, n-1));
+        System.out.println("Solve using Recursion Method : " + MCM(arr, 1, n - 1));
 
-            System.out.println("Solve using Memoization Method : " + MCM2(arr, 1, n-1, dp));
-      }
+        System.out.println("Solve using Memoization Method : " + MCM2(arr, 1, n - 1, dp));
+    }
 }
