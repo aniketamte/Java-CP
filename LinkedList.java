4ac937357317f297a20bddc7037a70c1bca91ffe
aniketamte <aniketamte123@gmail.com>
2025-03-28 18:26:19 +0530

    Revise the concept of LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -142,9 +142,23 @@ public class LinkedList {
 
     //helper function
     public int helper(Node head, int key){
+        if(head == null){
+            //Key doesn't Exist
+            return -1;
+        }
 
+        if(head.data == key){
+            return 0;
+        }
+
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+
+        return idx+1;
     }
-    
+
     //Recursive search in LL
     public int recursiveSearch(int key){
 
