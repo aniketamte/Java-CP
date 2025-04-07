09f6a1d477907d493b8cf8450730c181e7cb384a
aniketamte <aniketamte123@gmail.com>
2025-04-07 19:16:39 +0530

    Practice String Concept

--- Strings.java
+++ Strings.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 
 public class Strings {
 
+      //Find character index
+
       public static void printLetter(String str){
             for(int i=0; i<str.length(); i++){
                   System.out.print(str.charAt(i) + " ");
             }
             System.out.println();
+      }
+
+      //Check String is palindrome or not
+
+      public static boolean isPalindrome(String str){
+            for(int i=0; i<str.length()/2; i++){
+                  int n = str.length();
+                  if(str.charAt(i) != str.charAt(n-1-i)){
+                        //Not a palindrome
+                        return false;
+                  }
+            }
+            return true;
       }
 
     public static void main(String[] args) {
@@ -37,5 +52,8 @@ public class Strings {
         //System.out.println("Find Character at : " + fullName.charAt(8));
 
         printLetter("Find character index : " + fullName);
+
+        //Check palindrome
+        System.out.println(isPalindrome("raceacar"));
     }
 }
