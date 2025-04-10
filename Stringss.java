32d779fc4cdeeb487a8ffed55a25782467f25836
aniketamte <aniketamte123@gmail.com>
2025-04-10 10:20:57 +0530

    Practice

---
+++ Stringss.java
@@ -0,0 +1,10 @@
+public class Stringss{
+      public static void main(String[] args) {
+            String a = "Aniket";
+            String b = "Aniket";
+
+            // ==
+
+            System.out.println( a == b);
+      }
+}
