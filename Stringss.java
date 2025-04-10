b2f38e0fc7433d34fec26bfe4aad94dab94bace5
aniketamte <aniketamte123@gmail.com>
2025-04-10 10:29:06 +0530

    Practice

--- Stringss.java
+++ Stringss.java
@@ -1,4 +1,11 @@
 public class Stringss{
+      /*
+        1) Comarison Of String :-
+           -> == method :-
+              -> Checks if reference variable are pointing to same object
+              -> when you need to check only value use .equal method
+
+       */
       public static void main(String[] args) {
             String a = "Aniket";
             String b = "Aniket";
@@ -6,5 +13,10 @@ public class Stringss{
             // ==
 
             System.out.println( a == b);
+
+            String name1 = new String("Amte");
+            String name2 = new String("Amte");
+            System.out.println(name1 == name2);
+            System.out.println(name1.equals(name2));
       }
 }
