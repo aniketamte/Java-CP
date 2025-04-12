130a0af347003d58de5a00fb2dcb0adeddd09a47
aniketamte <aniketamte123@gmail.com>
2025-04-12 12:08:26 +0530

    Leetcode : 151 ===> Reverse a string

--- Leetcode151.java
+++ Leetcode151.java
@@ -3,11 +3,12 @@ public class Leetcode151 {
   //Reverse word
 
     public static String reverseWords(String s) {
-      String[] words = s.split(" +");
-      StringBuilder sb= new StringBuilder();
-      for(int i=words.length-1; i>=0; i--){
-            sb.append(words[i]);
-            sb.append(" ");
+      String words[] = s.split(" +");
+      StringBuilder sb = new StringBuilder();
+
+      for(int i=words.length - 1; i>=0; i--){
+        sb.append(words[i]);
+        sb.append(" ");
       }
       return sb.toString().trim();
     }
