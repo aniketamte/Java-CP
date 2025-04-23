4e9d7256874d4de461b385ffa0a5d0ef0885cf2c
aniketamte <aniketamte123@gmail.com>
2025-04-23 22:41:04 +0530

    Practice

---
+++ Arrays/RotateMatrixTest.java
@@ -0,0 +1,52 @@
+
+public class RotateMatrixTest {
+
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+
+    public void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    public void reverse(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0;
+            int right = matrix.length - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        RotateMatrixTest sol = new RotateMatrixTest();
+        sol.rotate(matrix);
+
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
