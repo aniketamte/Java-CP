90a809a5031e0e86c450b9140cdc1d2ea14f0f19
aniketamte <aniketamte123@gmail.com>
2025-04-23 22:46:06 +0530

    Practice

--- StackLearn/StackB.java
+++ StackLearn/StackB.java
@@ -1,39 +1,49 @@
+
 import java.util.ArrayList;
 
 public class StackB {
-      static class Stack{
-            static ArrayList<Integer> list = new ArrayList<>();
 
-            public static boolean isEmpty() {
-                  return list.size() == 0;
-            }
+    static class Stack {
 
-            //push
-            public static void push(int data){
-                  list.add(data);
-            }
+        static ArrayList<Integer> list = new ArrayList<>();
 
-            //pop
-            public static int pop(){
-                  int top = list.get(list.size() - 1);
-                  list.remove(list.size()-1);
-                  return top;
-            }
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
 
-            //peek --> return top
-            public static int peek() {
-                  return list.get(list.size() - 1);
-            }
-      }
-      public static void main(String[] args) {
-            Stack s = new Stack();
-            s.push(1);
-            s.push(2);
-            s.push(3);
+        //push
+        public static void push(int data) {
+            list.add(data);
+        }
 
-            while(!s.isEmpty()){
-                  System.out.println(s.peek());
-                  s.pop();
+        //pop
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
             }
-      }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        //peek --> return top
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }
