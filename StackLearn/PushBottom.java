b7de28ad9166f842fdf272cf0920f6db56a1a893
aniketamte <aniketamte123@gmail.com>
2025-04-24 13:26:00 +0530

    Practice

--- StackLearn/PushBottom.java
+++ StackLearn/PushBottom.java
@@ -14,15 +14,33 @@ public class PushBottom {
       s.push(top);
     }
 
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void printStack(Stack<Integer> s){
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
 
-        pushAtBottom(s, 4);
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
-        }
+        //pushAtBottom(s, 4);
+        // while (!s.isEmpty()) {
+        //     System.out.println(s.pop());
+        // }
+
+        //printStack(s);
+        reverseStack(s);
+        printStack(s);
     }
 }
