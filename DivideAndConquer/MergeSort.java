f74113b2ad0f4deacdc664000ef6d6bdac46eeba
aniketamte <aniketamte123@gmail.com>
2025-05-03 19:05:33 +0530

    Practice

--- DivideAndConquer/MergeSort.java
+++ DivideAndConquer/MergeSort.java
@@ -49,7 +49,7 @@ public class MergeSort{
             }
       }
       public static void main(String[] args) {
-            int arr[] = {6, 3, 9, 5, 2, 8};
+            int arr[] = {6, 3, 9, 5, 2, -2, 8};
             mergeSort(arr, 0, arr.length-1);
             prinArr(arr);
       }
