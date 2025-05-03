6a8f07f2397e71e5f426b6c137af51fc32f540dd
aniketamte <aniketamte123@gmail.com>
2025-05-04 00:05:36 +0530

    Practice

--- Binary Tree/height.java
+++ Binary Tree/height.java
@@ -21,6 +21,34 @@ public class height {
             int rh = heightOfTree(root.right);
             return Math.max(lh, rh) + 1;
       }
+      public static int count(Node root){
+            if(root == null){
+                  return 0;
+            }
+            int lcount = count(root.left);
+            int rcount = count(root.right);
+            return lcount + rcount + 1;
+      }
+      public static int sum(Node root){
+            if (root == null) {
+                  return 0;
+            }
+            int lsum = sum(root.left);
+            int rsum = sum(root.right);
+            return lsum + rsum + root.data;
+      }
+      public static int diameter(Node root){
+            if (root == null) {
+                  return 0;
+            }                   
+            int lDiam = diameter(root.left);
+            int rDiam = diameter(root.right);
+            int lHeight = heightOfTree(root.left);
+            int rHeight = heightOfTree(root.right);
+            int selfDiam = lHeight + rHeight + 1;
+
+            return Math.max(selfDiam, Math.max(lDiam, rDiam));
+      }
       public static void main(String[] args) {
             Node root = new Node(1);
             root.left = new Node(2);
@@ -31,5 +59,8 @@ public class height {
             root.right.right = new Node(7);
 
             System.out.println(heightOfTree(root));
+            System.out.println(count(root));
+            System.out.println(sum(root));
+            System.out.println(diameter(root));
       }
 }
