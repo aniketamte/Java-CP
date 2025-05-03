a1b7cc413261f0e2077a6851a91f5f51dbf45e40
aniketamte <aniketamte123@gmail.com>
2025-05-03 23:34:20 +0530

    Practice

--- Binary Tree/BinaryTreesB.java
+++ Binary Tree/BinaryTreesB.java
@@ -1,4 +1,7 @@
 
+import java.util.*;
+
+
 public class BinaryTreesB {
       static class Node {
             int data;
@@ -53,6 +56,34 @@ public class BinaryTreesB {
                   
             
             }
+            public static void levelOrder(Node root){
+                  if(root == null){
+                        return;
+                  }
+                  Queue<Node> q = new LinkedList<>();
+                  q.add(root);
+                  q.add(null);
+
+                  while(!q.isEmpty()){
+                        Node currNode = q.remove();
+                        if(currNode == null){
+                              System.out.println();
+                              if(q.isEmpty()){
+                                    break;
+                              } else{
+                                    q.add(null);
+                              }
+                        } else{
+                              System.out.print(currNode.data + " ");
+                              if(currNode.left != null){
+                                    q.add(currNode.left);
+                              }
+                              if (currNode.right != null) {
+                                    q.add(currNode.right);
+                              }
+                        }
+                  }
+            }
       }
 
        public static void main(String[] args) {
@@ -65,5 +96,7 @@ public class BinaryTreesB {
             tree.inOrder(root);
             System.out.println();
             tree.postOrder(root);
+            System.out.println();
+            tree.levelOrder(root);
       }
 }
