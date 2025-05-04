ee34e95cd21f771909ecb6127f8879d8ff2c5971
aniketamte <aniketamte123@gmail.com>
2025-05-04 23:00:29 +0530

    Practice

--- Binary Tree/LCA.java
+++ Binary Tree/LCA.java
@@ -1,7 +1,6 @@
 
 import java.util.ArrayList;
 
-
 public class LCA {
       static class Node {
             int data;
@@ -15,45 +14,67 @@ public class LCA {
             }
       }
 
-      public static boolean getPath(Node root, int n, ArrayList<Node> path){
-            if(root == null){
+      public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+            if (root == null) {
                   return false;
             }
             path.add(root);
-            if(root.data == n){
+            if (root.data == n) {
                   return true;
             }
 
             boolean foundLeft = getPath(root.left, n, path);
             boolean foundRight = getPath(root.right, n, path);
 
-            if(foundLeft || foundRight){
+            if (foundLeft || foundRight) {
                   return true;
             }
 
-            path.remove(path.size() -1);
+            path.remove(path.size() - 1);
 
             return false;
       }
 
-      public static Node lca(Node root, int n1, int n2){
+      public static Node lca(Node root, int n1, int n2) {
             ArrayList<Node> path1 = new ArrayList<>();
             ArrayList<Node> path2 = new ArrayList<>();
 
             getPath(root, n1, path1);
             getPath(root, n2, path2);
 
-            //last common ancesstor
+            // last common ancesstor
 
-            int i=0;
-            for(; i<path1.size() && i<path2.size(); i++){
-                  if(path1.get(i) != path2.get(i)){
+            int i = 0;
+            for (; i < path1.size() && i < path2.size(); i++) {
+                  if (path1.get(i) != path2.get(i)) {
                         break;
                   }
             }
 
-            Node lca = path1.get(i-1);
+            Node lca = path1.get(i - 1);
             return lca;
+      }
+
+      public static Node lca2(Node root, int n1, int n2){
+            // if (root == null) {
+            //       return null;
+            // }
+
+            if(root == null || root.data == n1 || root.data == n2){
+                  return root;
+            }
+
+            Node leftLca = lca2(root.left, n1, n2);
+            Node rightLca = lca2(root.right, n1, n2);
+
+            if(rightLca == null){
+                  return leftLca;
+            }
+
+            if(leftLca == null){
+                  return rightLca;
+            }
+            return root;
       }
 
       public static void main(String[] args) {
@@ -65,7 +86,7 @@ public class LCA {
             root.right.left = new Node(6);
             root.right.right = new Node(7);
 
-            int n1=4, n2=7;
-            System.out.println(lca(root, n1, n2).data);
+            int n1 = 4, n2 = 7;
+            System.out.println(lca2(root, n1, n2).data);
       }
 }
