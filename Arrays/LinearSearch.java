222b72fa2c3759c3932f34fa3e4b995b7d2fd513
Aniket Vijay Amte <134209470+aniketamte@users.noreply.github.com>
2025-05-13 21:24:08 +0530

    LinearSearch.java

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -8,7 +8,7 @@ public class LinearSearch {
             return -1;
       }
       public static void main(String[] args) {
-            int arr[] = {1, 2, 3, 4, 5};
+            int arr[] = {1, 2, 3, 4, 5}; 
             int num = 3;
             System.out.println(linearSearch(arr, num));
       }
