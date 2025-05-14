8123e04b6adcdd2acaa1a968d7e5878a411adda3
Aniket Vijay Amte <134209470+aniketamte@users.noreply.github.com>
2025-05-14 21:18:17 +0530

    largestElement.java

--- Arrays/largestElement.java
+++ Arrays/largestElement.java
@@ -1,7 +1,6 @@
 public class largestElement{
       public static int large(int arr[]){
             int max = arr[0];
-
             for(int i=1; i<arr.length; i++){
                   if(arr[i] > max){
                         max = arr[i];
