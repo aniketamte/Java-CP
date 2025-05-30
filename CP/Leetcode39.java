6fbf46634a9da4ac7164d1779176bb1ee7110399
aniketamte <aniketamte123@gmail.com>
2025-05-30 16:59:35 +0530

    Practice

--- CP/Leetcode39.java
+++ CP/Leetcode39.java
@@ -6,30 +6,51 @@ public class Leetcode39 {
 
       //Leetcode : 39 --->  Combination Sum
 
-    private static void findCombinations(int ind, int arr[], int target, List<List<Integer>> ans, List<Integer> ds) {
-        // Base case: if the target is met, add the combination to the answer
-        if (target == 0) {
-            ans.add(new ArrayList<>(ds));
+    // private static void findCombinations(int ind, int arr[], int target, List<List<Integer>> ans, List<Integer> ds) {
+    //     // Base case: if the target is met, add the combination to the answer
+    //     if (target == 0) {
+    //         ans.add(new ArrayList<>(ds));
+    //         return;
+    //     }
+
+    //     // Ensure that the index does not go out of bounds
+    //     if (ind >= arr.length) {
+    //         return;
+    //     }
+
+    //     // If the current number can be used, include it
+    //     if (arr[ind] <= target) {
+    //         ds.add(arr[ind]);
+    //         findCombinations(ind, arr, target - arr[ind], ans, ds); // Stay at the same index for unlimited usage
+    //         ds.remove(ds.size() - 1); // Backtrack
+    //     }
+
+    //     // Move to the next index
+    //     findCombinations(ind + 1, arr, target, ans, ds);
+    // }
+
+    // public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     findCombinations(0, candidates, target, ans, new ArrayList<>());
+    //     return ans;
+    // }
+
+    private static void findCombinations(int index, int arr[], int target, List<List<Integer>> ans, List<Integer> ds){
+        if(index == arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
             return;
         }
-
-        // Ensure that the index does not go out of bounds
-        if (ind >= arr.length) {
-            return;
+        if(arr[index] <= target){
+            ds.add(arr[index]);
+            findCombinations(index, arr, target - arr[index], ans, ds);
+            ds.remove(ds.size() - 1);
         }
-
-        // If the current number can be used, include it
-        if (arr[ind] <= target) {
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds); // Stay at the same index for unlimited usage
-            ds.remove(ds.size() - 1); // Backtrack
-        }
-
-        // Move to the next index
-        findCombinations(ind + 1, arr, target, ans, ds);
+        findCombinations(index + 1, arr, target, ans, ds);
     }
 
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public static List<List<Integer>> combinationSum(int[] candidates, int target){
         List<List<Integer>> ans = new ArrayList<>();
         findCombinations(0, candidates, target, ans, new ArrayList<>());
         return ans;
