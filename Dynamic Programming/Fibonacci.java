d4b52a39d60f101389189f63f9e92f7dfc6580e5
aniketamte <aniketamte123@gmail.com>
2025-06-03 15:48:44 +0530

    DP Practice

--- Dynamic Programming/Fibonacci.java
+++ Dynamic Programming/Fibonacci.java
@@ -1,8 +1,6 @@
 public class Fibonacci {
       public static void fib(int n, int fib[]){
-            // if(n==0 || n==1){
-            //       return;
-            // }
+            // Bottom - Up Approach
             for(int i=2; i<=n; i++){
                   fib[i] = fib[i-1] + fib[i-2];
             }
