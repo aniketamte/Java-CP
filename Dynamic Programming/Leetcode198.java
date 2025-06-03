3ee1bc96533ebbe652cefc88dbb3ce7757ec4875
aniketamte <aniketamte123@gmail.com>
2025-06-03 17:12:34 +0530

    DP Practice

--- Dynamic Programming/Leetcode198.java
+++ Dynamic Programming/Leetcode198.java
@@ -1,17 +1,28 @@
+
+import java.util.Arrays;
+
 public class Leetcode198 {
       //Recursive Approach
       public static int rob(int[] nums){
             int n = nums.length;
-            return solve(nums, 0, n);
+            int dp[] = new int[n+1];
+            Arrays.fill(dp, -1);
+            return solve(nums, 0, dp);
       }
-      public static int solve(int nums[], int i, int n){
+      public static int solve(int nums[], int i, int dp[]){
             if(i >= nums.length){
                   return 0;
             }
-            int steal = nums[i] + solve(nums, i+2, n);
-            int skip = solve(nums, i+1, n);
 
-            return Math.max(steal, skip);
+            if(dp[i] != -1){
+                  return dp[i];
+            }
+
+            int steal = nums[i] + solve(nums, i+2, dp);
+            int skip = solve(nums, i+1, dp);
+
+            dp[i] = Math.max(steal, skip);
+            return dp[i];
       }
       public static void main(String[] args) {
             int nums[] = {1,2,3,1};
