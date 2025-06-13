25b56075ca9af6e20fe70df409e499e92376aa27
aniketamte <aniketamte123@gmail.com>
2025-06-13 08:48:17 +0530

    Graph

--- Binary Search Tree/Leetcode173.java
+++ Binary Search Tree/Leetcode173.java
@@ -1,3 +1,12 @@
 public class Leetcode173 {
-      
+      static class TreeNode {
+            int val;
+            TreeNode left, right;
+
+            TreeNode(int val) {
+                  this.val = val;
+                  this.left = null;
+                  this.right = null;
+            }
+      }
 }
