b18f8d8833101a8e88911cfa3537a119ccfc1dfd
aniketamte <aniketamte123@gmail.com>
2025-06-20 13:13:31 +0530

    Practice

--- Arrays/Arrays-III/Leetcode229.java
+++ Arrays/Arrays-III/Leetcode229.java
@@ -2,39 +2,46 @@ import java.util.*;
 
 public class Leetcode229 {
       public static List<Integer> majorityElement(int[] nums) {
-            int count1 = 0, count2 = 0;
-            int maj1 = 0, maj2 = 0;
-            int n= nums.length;
+            int n = nums.length;
 
-            for(int i=1; i<n; i++){
-                  if(nums[i] == maj1){
+            Integer maj1 = null;
+            int count1 = 0;
+
+            Integer maj2 = null;
+            int count2 = 0;
+
+            for (int num : nums) {
+                  if (maj1 != null && num == maj1) {
                         count1++;
-                  } else if(nums[i] == maj2){
+                  } else if (maj2 != null && num == maj2) {
                         count2++;
-                  } else if(count1 == 0){
-                        maj1 = nums[i];
+                  } else if (count1 == 0) {
+                        maj1 = num;
                         count1 = 1;
-                  } else if(count2 == 0){
-                        maj2 = nums[i];
+                  } else if (count2 == 0) {
+                        maj2 = num;
                         count2 = 1;
                   } else {
                         count1--;
                         count2--;
                   }
             }
-            List<Integer> result = new ArrayList<>();
-            int freq1 = 0;
-            int freq2 = 0;
-            for(int num : nums){
-                  if(num == maj1) freq1++;
-                  else if(num == maj2) freq2++;
+
+            count1 = 0;
+            count2 = 0;
+            for (int num : nums) {
+                  if (maj1 != null && num == maj1) {
+                        count1++;
+                  } else if (maj2 != null && num == maj2) {
+                        count2++;
+                  }
             }
 
-            if(freq1 > Math.floor(n/3)){
+            List<Integer> result = new ArrayList<>();
+            if (count1 > Math.floor(n / 3)) {
                   result.add(maj1);
             }
-
-            if(freq2 > Math.floor(n/3)){
+            if (count2 > Math.floor(n / 3)) {
                   result.add(maj2);
             }
 
