36cbe944f4d97b64f5efe752b971f18b3b327c13
aniketamte <aniketamte123@gmail.com>
2025-06-25 07:23:45 +0530

    Practice

---
+++ LinkedList/SDE/Leetcode141.java
@@ -0,0 +1,56 @@
+
+public class Leetcode141 {
+      // Leetcode : 141
+      // 03-03-2025
+      // March
+
+      class ListNode {
+
+            int val;
+            ListNode next;
+
+            ListNode(int x) {
+                  val = x;
+                  next = null;
+            }
+
+      }
+
+      public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                  return false;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast.next != null && fast.next.next != null) {
+                  fast = fast.next.next;
+                  slow = slow.next;
+
+                  if (slow == fast) {
+                        return true;
+                  }
+            }
+
+            return false;
+      }
+
+      public static void main(String[] args) {
+            Leetcode141 solution = new Leetcode141();
+
+            // Creating a linked list with a cycle: [3,2,0,-4], tail connects to index 1
+            ListNode head = solution.new ListNode(3);
+            ListNode second = solution.new ListNode(2);
+            ListNode third = solution.new ListNode(0);
+            ListNode fourth = solution.new ListNode(-4);
+
+            head.next = second;
+            second.next = third;
+            third.next = fourth;
+            fourth.next = second; // Creating a cycle at node index 1
+
+            boolean result = solution.hasCycle(head);
+            System.out.println("Does the linked list have a cycle? " + result);
+      }
+}
