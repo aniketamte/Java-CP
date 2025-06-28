46c2edae1b1d91721f64c36f33c14b6e817ff535
aniketamte <aniketamte123@gmail.com>
2025-06-28 16:41:04 +0530

    Practice

--- Binary Search/Leetcode4.java
+++ Binary Search/Leetcode4.java
@@ -93,10 +93,45 @@ public class Leetcode4 {
             return (element1 + element2) / 2.0; // even
       }
 
+      public static double BinarySearch(int[] nums1, int[] nums2){
+            if (nums1.length > nums2.length) {
+                  return findMedianSortedArrays(nums2, nums1);
+            }
+
+            int m = nums1.length;
+            int n = nums2.length;
+
+            int low = 0, high = m;
+            while (low <= high) {
+                  int Px = low + (high - low) / 2;
+                  int Py = (m + n + 1) / 2 - Px;
+
+                  int x1 = (Px == 0) ? Integer.MIN_VALUE : nums1[Px - 1];
+                  int x3 = (Px == m) ? Integer.MAX_VALUE : nums1[Px];
+
+                  int x2 = (Py == 0) ? Integer.MIN_VALUE : nums2[Py - 1];
+                  int x4 = (Py == n) ? Integer.MAX_VALUE : nums2[Py];
+
+                  if (x1 <= x4 && x2 <= x3) {
+                        if ((m + n) % 2 == 0) {
+                              return (Math.max(x1, x2) + Math.min(x3, x4)) / 2.0;
+                        }
+                        return Math.max(x1, x2);
+                  } else if (x1 > x4) {
+                        high = Px - 1;
+                  } else {
+                        low = Px + 1;
+                  }
+            }
+
+            return -1;
+      }
+
       public static void main(String[] args) {
             int[] nums1 = { 1, 3, 4, 5 };
             int[] nums2 = { 2, 3, 9, 6 };
             System.out.println(findMedianSortedArrays(nums1, nums2));
             System.out.println(BetterApproach(nums1, nums2));
+            System.out.println(BinarySearch(nums1, nums2));
       }
 }
