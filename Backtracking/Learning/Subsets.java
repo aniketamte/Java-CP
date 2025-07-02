992bd41243311c4d20a94149ccb0af6f5035c4e4
aniketamte <aniketamte123@gmail.com>
2025-07-02 16:06:53 +0530

    Practice

---
+++ Backtracking/Learning/Subsets.java
@@ -0,0 +1,19 @@
+public class Subsets {
+      public static void findSubsets(String str, String ans, int i){
+            if(i == str.length()){
+                  if(ans.length() == 0){
+                        System.out.println("NULL");
+                  }
+                  System.out.println(ans);
+                  return;
+            }
+            //Yes Choice
+            findSubsets(str, ans+str.charAt(i), i+1);
+            //No Choice
+            findSubsets(str, ans, i+1);
+      }
+      public static void main(String[] args) {
+            String str = "abc";
+            findSubsets(str, "", 0);
+      }
+}
