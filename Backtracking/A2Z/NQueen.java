539819eb585108de09ac484dd6a82d45a2d64f2d
aniketamte <aniketamte123@gmail.com>
2025-07-02 23:59:58 +0530

    Practice

--- Backtracking/A2Z/NQueen.java
+++ Backtracking/A2Z/NQueen.java
@@ -5,10 +5,36 @@ public class NQueen {
                   return;
             }
             for(int j=0; j<board.length; j++){
-                  board[row][j] = 'Q';
-                  nQueen(board, row+1);  //Function call
-                  board[row][j] = 'X';   //Backtracking step
+                  if(isSafe(board, row, j)){
+                        board[row][j] = 'Q';
+                        nQueen(board, row + 1); // Function call
+                        board[row][j] = 'X'; // Backtracking step
+                  }
             }
+      }
+
+      public static boolean isSafe(char board[][], int row, int col){
+            //vertical up
+            for(int i=row-1; i>=0; i--){
+                  if(board[i][col] == 'Q'){
+                        return false;
+                  }
+            }
+
+            //diagonal left up
+            for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+                  if(board[i][j] == 'Q'){
+                        return false;
+                  }
+            }
+
+            //diagonal right up
+            for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
+                  if(board[i][j] == 'Q'){
+                        return false;
+                  }
+            }
+            return true;
       }
 
       public static void printBoard(char board[][]){
@@ -20,8 +46,9 @@ public class NQueen {
                   System.out.println();
             }
       }
+
       public static void main(String[] args){
-            int n = 2;
+            int n = 4;
             char board[][] = new char[n][n];
 
             //initialize
