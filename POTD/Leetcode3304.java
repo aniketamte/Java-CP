92568281292edcd78eeabd0c3848ee04e4fecaa9
aniketamte <aniketamte123@gmail.com>
2025-07-03 22:48:33 +0530

    Practice

--- POTD/Leetcode3304.java
+++ POTD/Leetcode3304.java
@@ -3,7 +3,25 @@ public class Leetcode3304 {
             int shift = Integer.bitCount(k - 1);
             return (char) ('a' + shift);
       }
+
+      public static char kthCharacter2(int k) {
+            int idx = k - 1;
+            StringBuilder result = new StringBuilder();
+            result.append('a');
+
+            while (result.length() < k) {
+                  int n = result.length();
+                  for (int i = 0; i < n; i++) {
+                        char ch = result.charAt(i) == 'z' ? 'a' : (char) (result.charAt(i) + 1);
+                        result.append(ch);
+                  }
+            }
+
+            return result.charAt(idx);
+      }
+
       public static void main(String[] args) {
             System.out.println(kthCharacter(5));
+            System.out.println(kthCharacter2(5));
       }
 }
