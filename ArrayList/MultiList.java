2895e9d3c4438cd011fb1e8b3b4562feb9d701a5
aniketamte <aniketamte123@gmail.com>
2025-07-07 22:47:23 +0530

    Practice

--- ArrayList/MultiList.java
+++ ArrayList/MultiList.java
@@ -23,6 +23,16 @@ public class MultiList {
 
             mainList.add(list2);
 
+            //System.out.println(mainList);
+
+            for(int i=0; i<mainList.size(); i++){
+                  ArrayList<Integer> currList = mainList.get(i);
+                  for(int j=0; j<currList.size(); j++){
+                        System.out.print(currList.get(j) + " ");
+                  }
+                  System.out.println();
+            }
+
             System.out.println(mainList);
       }
 }
