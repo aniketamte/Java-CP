118b4b06ec08f8dd73bc48a8d859ff34bc35bc6e
aniketamte <aniketamte123@gmail.com>
2025-07-07 14:54:50 +0530

    Practice

--- DivideAndConquer/Revision.java
+++ DivideAndConquer/Revision.java
@@ -45,9 +45,43 @@ public class Revision {
                   arr[i] = temp[k];
             }
       }
+
+      public static void quickSort(int arr[], int si, int ei){
+            //Base Case
+            if(si >= ei){
+                  return;
+            }
+
+            //Last element as pivot
+            int pIdx = partition(arr, si, ei);
+            quickSort(arr, si, pIdx-1); //Left 
+            quickSort(arr, pIdx+1, ei); //Right
+      }
+
+      public static int partition(int arr[], int si, int ei){
+            int pivot = arr[ei]; //Last element as pivot
+            int i = si-1; //Idx for smaller elements
+            for(int j=si; j<ei; j++){
+                  if(arr[j] <= pivot){
+                        i++;
+                        int temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                  }
+            }
+            i++;
+            int temp = pivot;
+            arr[ei] = arr[i];
+            arr[i] = temp;
+            return i;
+      }
+
      public static void main(String[] args) {
             int arr[] = {6,3,9,5,2,8};
             mergeSort(arr, 0, arr.length-1);
+            printArr(arr);
+            System.out.println("Using Quick sort");
+            quickSort(arr, 0, arr.length-1);
             printArr(arr);
       }
 }
