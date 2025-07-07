e319fbc3e4dc5a6a2affb682f8469798535b8e57
aniketamte <aniketamte123@gmail.com>
2025-07-07 14:31:27 +0530

    Practice

--- DivideAndConquer/Revision.java
+++ DivideAndConquer/Revision.java
@@ -10,45 +10,42 @@ public class Revision {
             if(si >= ei){
                   return;
             }
-            int mid = si + (ei-si)/2;
-            mergeSort(arr, si, mid);  //Left Part
-            mergeSort(arr, mid+1, ei);  //Right part
+            int mid = (si + ei) / 2;
+            mergeSort(arr, si, mid);
+            mergeSort(arr, mid+1, ei);
             merge(arr, si, mid, ei);
       }
 
       public static void merge(int arr[], int si, int mid, int ei){
             int temp[] = new int[ei-si+1];
-            int i = si;  //iterator for left part
-            int j = mid+1;  //iterator for right part
-            int k = 0;  //iterator for temp array
+            int i=si; //Idx for first sorted part(left part)
+            int j=mid+1; //Idx for second sorted part(right part)
+            int k=0; //Idx for temp array
 
-            while(i <= mid && j <= ei){
-                  if(arr[i] < arr[j]){
-                        temp[k] = arr[i];
-                        i++;
+            while(i<= mid && j<= ei){
+                  if(arr[i] <= arr[j]){
+                        temp[k++] = arr[i++];
                   } else{
-                        temp[k] = arr[j];
-                        j++;
+                        temp[k++] = arr[j++];
                   }
-                  k++;
             }
 
-            //Left part remaining elements
-            while(i <= mid){
-                  temp[k++] = arr[i++];
+            //copy remaining elements of left part
+            while(i<=mid){
+                  temp[k++] = arr[i++]; 
             }
 
-            //Right part remaining elements
-            while(j <= ei){
+            //copy remaining elements of right part
+            while(j<=ei){
                   temp[k++] = arr[j++];
             }
 
-            //copy temp array to original array
+            //copy elements from temp array to original array
             for(k=0, i=si; k<temp.length; k++, i++){
                   arr[i] = temp[k];
             }
       }
-      public static void main(String[] args) {
+     public static void main(String[] args) {
             int arr[] = {6,3,9,5,2,8};
             mergeSort(arr, 0, arr.length-1);
             printArr(arr);
