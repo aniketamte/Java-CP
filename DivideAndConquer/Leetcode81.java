0c7caee15f813d97ae1f449938b0d04e2e01d754
aniketamte <aniketamte123@gmail.com>
2025-07-07 15:26:14 +0530

    Practice

--- DivideAndConquer/Leetcode81.java
+++ DivideAndConquer/Leetcode81.java
@@ -32,5 +32,6 @@ public class Leetcode81 {
       public static void main(String[] args) {
             int[] nums = {4,5,6,7,0,1,2};
             int target = 0;
+            System.out.println(search(nums, target));
       }
 }
