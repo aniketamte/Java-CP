8cc557722b1c7480259e333e84aed5134880cd54
aniketamte <aniketamte123@gmail.com>
2025-07-12 23:58:06 +0530

    Practice

---
+++ LinkedList/A2Z/Leetcode2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Leetcode2 {
+      public class ListNode {
+            int val;
+            ListNode next;
+
+            ListNode() {
+            }
+
+            ListNode(int val) {
+                  this.val = val;
+            }
+
+            ListNode(int val, ListNode next) {
+                  this.val = val;
+                  this.next = next;
+            }
+      }
+
+      public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+      }
+
+      public static void main(String[] args) {
+
+      }
+}
---
+++ LinkedList/DLL/deleteOccurence.java
@@ -0,0 +1,11 @@
+public class deleteOccurence {
+      public static class Node {
+            int data;
+            Node next;
+
+            public Node(int d) {
+                  data = d;
+                  next = null;
+            }
+      }
+}
