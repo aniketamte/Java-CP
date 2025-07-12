2d97c55c2c8de5639fde8c5ec3d667d9b60ad8e6
aniketamte <aniketamte123@gmail.com>
2025-07-13 00:05:01 +0530

    Practice

--- LinkedList/A2Z/Leetcode2.java
+++ LinkedList/A2Z/Leetcode2.java
@@ -1,7 +1,5 @@
-import java.util.*;
-
 public class Leetcode2 {
-      public class ListNode {
+      public static class ListNode {
             int val;
             ListNode next;
 
@@ -19,10 +17,56 @@ public class Leetcode2 {
       }
 
       public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode dummyNode = new ListNode(-1);
+            ListNode curr = dummyNode;
+            int carry = 0;
 
+            while (l1 != null || l2 != null) {
+                  int sum = carry;
+
+                  if (l1 != null) {
+                        sum += l1.val;
+                        l1 = l1.next;
+                  }
+
+                  if (l2 != null) {
+                        sum += l2.val;
+                        l2 = l2.next;
+                  }
+
+                  carry = sum / 10;
+                  curr.next = new ListNode(sum % 10);
+                  curr = curr.next;
+            }
+
+            if (carry != 0) {
+                  curr.next = new ListNode(carry);
+            }
+
+            return dummyNode.next;
+      }
+
+      // Helper to print a ListNode
+      public static void printList(ListNode head) {
+            while (head != null) {
+                  System.out.print(head.val);
+                  if (head.next != null)
+                        System.out.print(" -> ");
+                  head = head.next;
+            }
+            System.out.println();
       }
 
       public static void main(String[] args) {
+            // l1 = [2,4,3]
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
 
+            // l2 = [5,6,4]
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+            ListNode result = addTwoNumbers(l1, l2);
+
+            System.out.print("Output: ");
+            printList(result); // Expected: 7 -> 0 -> 8
       }
 }
