83bbc15ae5708f7ddfd9860bb5168c5352133ebb
aniketamte <aniketamte123@gmail.com>
2025-07-17 18:06:46 +0530

    Practice

--- Queue/KK/InBuiltExample.java
+++ Queue/KK/InBuiltExample.java
@@ -12,9 +12,23 @@ public class InBuiltExample {
             que.add(70);
             que.add(80);
             que.add(90);
+
             System.out.println(que);
             System.out.println(que.peek());
             System.out.println(que.remove());
             System.out.println(que);
+
+            //Doubly ended queue - Deque
+            Deque<Integer> deque = new ArrayDeque<>();
+            deque.add(10);
+            deque.add(20);
+            deque.add(30);
+            deque.add(40);
+            deque.add(50);
+            System.out.println(deque);
+            System.out.println(deque.poll());
+            System.out.println(deque.offer(900));
+            System.err.println(deque);
+            System.out.println(deque.peek());
       }
 }
