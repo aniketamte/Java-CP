23c156acc9d2630d64e01c390b2f966f8701d082
aniketamte <aniketamte123@gmail.com>
2025-07-22 19:04:00 +0530

    Practice

--- Heaps/Basic.java
+++ Heaps/Basic.java
@@ -2,8 +2,10 @@ import java.util.PriorityQueue;
 
 public class Basic {
       public static void main(String[] args) {
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());  //print in reverse order
 
+            PriorityQueue<Integer> pq = new PriorityQueue<>(); //print in ascending order
+            
             pq.add(3);
             pq.add(4);
             pq.add(1);
