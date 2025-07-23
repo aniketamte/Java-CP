044c2f4619dcccc395f2431ef85035fecfc95d05
aniketamte <aniketamte123@gmail.com>
2025-07-23 18:11:30 +0530

    Practice

---
+++ Core Subjects/Oop/Day 2/human.java
@@ -0,0 +1,13 @@
+public class human{
+      int age;
+      String name;
+      int salary;
+      boolean isMarried;
+
+      public human(int age, String name, int salary, boolean isMarried) {
+          this.age = age;
+          this.name = name;
+          this.salary = salary;
+          this.isMarried = isMarried;
+      }
+}
---
+++ Core Subjects/Oop/Day 2/main.java
@@ -0,0 +1,10 @@
+public class main {
+      public static void main(String[] args) {
+            human aniket = new human(25, "Aniket", 50000, false);
+            System.out.println("Name: " + aniket.name);
+            System.out.println("Age: " + aniket.age);
+            System.out.println("Salary: " + aniket.salary);
+            System.out.println("Married: " + aniket.isMarried);
+            System.out.println("Aniket is a human.");
+      }
+}
