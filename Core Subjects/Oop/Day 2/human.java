6959b9140b8cb21a11a0bc6e9c6f22921915a1e4
aniketamte <aniketamte123@gmail.com>
2025-07-23 18:22:56 +0530

    Practice

--- Core Subjects/Oop/Day 2/human.java
+++ Core Subjects/Oop/Day 2/human.java
@@ -3,11 +3,13 @@ public class human{
       String name;
       int salary;
       boolean isMarried;
+      static long population;
 
       public human(int age, String name, int salary, boolean isMarried) {
           this.age = age;
           this.name = name;
           this.salary = salary;
           this.isMarried = isMarried;
+          human.population += 1; // Increment population for each new human instance
       }
 }
--- Core Subjects/Oop/Day 2/main.java
+++ Core Subjects/Oop/Day 2/main.java
@@ -1,10 +1,15 @@
 public class main {
       public static void main(String[] args) {
             human aniket = new human(25, "Aniket", 50000, false);
+
+            human amte = new human(25, "Amte", 56000, false);
+
             System.out.println("Name: " + aniket.name);
             System.out.println("Age: " + aniket.age);
             System.out.println("Salary: " + aniket.salary);
             System.out.println("Married: " + aniket.isMarried);
-            System.out.println("Aniket is a human.");
+            
+            System.out.println("Population: " + aniket.population);
+            System.out.println("Population: " + amte.population);
       }
 }
