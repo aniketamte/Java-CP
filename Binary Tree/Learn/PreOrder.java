b9aa1d8e4b3b51c5e6b4eb2b0c95e58114f34199
aniketamte <aniketamte123@gmail.com>
2025-07-30 12:32:50 +0530

    Practice

--- Binary Tree/Learn/PreOrder.java
+++ Binary Tree/Learn/PreOrder.java
@@ -21,11 +21,46 @@ public class PreOrder{
                   newNode.right = buildTree(nodes);
                   return newNode;
             }
+            public static void preOrder(Node root) {
+                  if(root == null) {
+                        return;
+                  }
+                  System.out.print(root.data + " ");
+                  preOrder(root.left);
+                  preOrder(root.right);
+            }
+
+            public static void postOrder(Node root) {
+                  if(root == null) {
+                        return;
+                  }
+                  postOrder(root.left);
+                  postOrder(root.right);
+                  System.out.print(root.data + " ");
+            }
+
+            public static void inOrder(Node root) {
+                  if(root == null) {
+                        return;
+                  }
+                  inOrder(root.left);
+                  System.out.print(root.data + " ");
+                  inOrder(root.right);
+            }
       }
       public static void main(String[] args) {
             int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1,-1};
             BinaryTree tree = new BinaryTree();
             Node root = tree.buildTree(nodes);
-            System.out.println("PreOrder Traversal of the Binary Tree:" + root.data);
+            System.out.println(root.data);
+            System.out.print("PreOrder: ");
+            tree.preOrder(root);
+            System.out.println();
+            System.out.print("PostOrder: ");
+            tree.postOrder(root);
+            System.out.println();
+            System.out.print("InOrder: ");
+            tree.inOrder(root);
+            System.out.println();
       }
 }
