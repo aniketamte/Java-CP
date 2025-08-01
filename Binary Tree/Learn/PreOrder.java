72308d71b972346304dcb143c164085f72a20722
aniketamte <aniketamte123@gmail.com>
2025-08-01 22:30:16 +0530

    Practice

--- Binary Tree/Learn/PreOrder.java
+++ Binary Tree/Learn/PreOrder.java
@@ -110,7 +110,6 @@ public class PreOrder {
                   return sumOfNodes(root.left) + sumOfNodes(root.right) + root.data;
             }
       }
-
       public static void main(String[] args) {
             int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
             BinaryTree tree = new BinaryTree();
