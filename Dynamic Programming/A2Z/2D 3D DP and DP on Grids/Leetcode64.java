3fe2559570cd4c03398e01aa8f068619fafce86d
aniketamte <aniketamte123@gmail.com>
2025-08-07 23:19:11 +0530

    Leetcode64 --> Bottom-up  Approach

--- Dynamic Programming/A2Z/2D 3D DP and DP on Grids/Leetcode64.java
+++ Dynamic Programming/A2Z/2D 3D DP and DP on Grids/Leetcode64.java
@@ -7,7 +7,7 @@ public class Leetcode64 {
                   return grid[i][j];
             }
 
-            if(dp[i][j] != -1){
+            if (dp[i][j] != -1) {
                   return dp[i][j];
             }
 
@@ -18,7 +18,8 @@ public class Leetcode64 {
                   // last col --> only go down
                   return dp[i][j] = grid[i][j] + solve(grid, i + 1, j, m, n, dp);
             } else {
-                  return dp[i][j] = grid[i][j] + Math.min(solve(grid, i + 1, j, m, n, dp), solve(grid, i, j + 1, m, n, dp));
+                  return dp[i][j] = grid[i][j]
+                              + Math.min(solve(grid, i + 1, j, m, n, dp), solve(grid, i, j + 1, m, n, dp));
             }
       }
 
@@ -26,15 +27,42 @@ public class Leetcode64 {
             int m = grid.length;
             int n = grid[0].length;
 
-            int dp[][] = new int[m+1][n+1];
-            for(int row[] : dp){
+            int dp[][] = new int[m + 1][n + 1];
+            for (int row[] : dp) {
                   Arrays.fill(row, -1);
             }
             return solve(grid, 0, 0, m, n, dp);
       }
 
+      public static int minPathSum2(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+
+            int dp[][] = new int[m + 1][n + 1];
+
+            dp[0][0] = grid[0][0];
+
+            // Fill first row
+            for (int col = 1; col < n; col++) {
+                  dp[0][col] = grid[0][col] + dp[0][col - 1];
+            }
+
+            // Fill first col
+            for (int row = 1; row < m; row++) {
+                  dp[row][0] = grid[row][0] + dp[row - 1][0];
+            }
+
+            for (int i = 1; i < m; i++) {
+                  for (int j = 1; j < n; j++) {
+                        dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                  }
+            }
+            return dp[m - 1][n - 1];
+      }
+
       public static void main(String[] args) {
             int grid[][] = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
             System.out.println(minPathSum(grid));
+            System.out.println(minPathSum2(grid));
       }
 }
