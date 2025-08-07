23664093b4f20c1035053847ead4a7c64ba718f9
aniketamte <aniketamte123@gmail.com>
2025-08-07 19:27:25 +0530

    Leetcode62 ==> Recursion + Memoization

--- Dynamic Programming/A2Z/2D 3D DP and DP on Grids/Leetcode62.java
+++ Dynamic Programming/A2Z/2D 3D DP and DP on Grids/Leetcode62.java
@@ -1,21 +1,36 @@
+
+import java.util.Arrays;
+
 public class Leetcode62{
-      public static int solve(int i, int j, int m, int n){
+      public static int solve(int i, int j, int m, int n, int dp[][]){
+            if (i == m - 1 && j == n - 1) {
+                  return 1; // found 1 path
+            }
+
             if(i<0 || i>=m || j<0 || j>=n){
-                  return 0;
+                  return 0; //Invalid path => no path found
             }
 
-            if(i == m-1 && j == n-1){
-                  return 1;
+            if(dp[i][j] != -1){
+                  return dp[i][j];
             }
 
-            int right = solve(i, j+1, m, n);
-            int down = solve(i+1, j, m, n);
+            int right = solve(i, j+1, m, n, dp);
+            int down = solve(i+1, j, m, n, dp);
 
-            return right + down;
+            dp[i][j] = right + down;
+            
+            return dp[i][j];
       }
+
       public static int uniquePaths(int m, int n){
-            return solve(0, 0, m, n);
+            int dp[][] = new int[m][n];
+            for(int row[] : dp){
+                  Arrays.fill(row, -1);
+            }
+            return solve(0, 0, m, n, dp);
       }
+
       public static void main(String[] args) {
             int m = 3, n = 7;
             System.out.println(uniquePaths(m, n));
