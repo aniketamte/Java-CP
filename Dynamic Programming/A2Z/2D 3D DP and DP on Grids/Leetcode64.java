9fd1e4068cb057455e6bd338a13edd89bf61c7a4
aniketamte <aniketamte123@gmail.com>
2025-08-07 22:50:21 +0530

    Leetcode64 --> Recursion Approach

---
+++ Dynamic Programming/A2Z/2D 3D DP and DP on Grids/Leetcode64.java
@@ -0,0 +1,27 @@
+public class Leetcode64 {
+      public static int solve(int grid[][], int i, int j, int m, int n) {
+            if (i == m - 1 && j == n - 1) {
+                  return grid[i][j];
+            }
+            if (i == m - 1) {
+                  // last row --> only go right
+                  return grid[i][j] + solve(grid, i, j + 1, m, n);
+            } else if (j == n - 1) {
+                  // last col --> only go down
+                  return grid[i][j] + solve(grid, i + 1, j, m, n);
+            } else {
+                  return grid[i][j] + Math.min(solve(grid, i + 1, j, m, n), solve(grid, i, j + 1, m, n));
+            }
+      }
+
+      public static int minPathSum(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            return solve(grid, 0, 0, m, n);
+      }
+
+      public static void main(String[] args) {
+            int grid[][] = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+            System.out.println(minPathSum(grid));
+      }
+}
