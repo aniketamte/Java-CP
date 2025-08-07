87551f2c76edcc940ee412d7b99703d1d5a3cafd
aniketamte <aniketamte123@gmail.com>
2025-08-07 23:05:17 +0530

    Leetcode64 --> Memoization  Approach

--- Dynamic Programming/A2Z/2D 3D DP and DP on Grids/Leetcode64.java
+++ Dynamic Programming/A2Z/2D 3D DP and DP on Grids/Leetcode64.java
@@ -1,23 +1,36 @@
+
+import java.util.Arrays;
+
 public class Leetcode64 {
-      public static int solve(int grid[][], int i, int j, int m, int n) {
+      public static int solve(int grid[][], int i, int j, int m, int n, int dp[][]) {
             if (i == m - 1 && j == n - 1) {
                   return grid[i][j];
             }
+
+            if(dp[i][j] != -1){
+                  return dp[i][j];
+            }
+
             if (i == m - 1) {
                   // last row --> only go right
-                  return grid[i][j] + solve(grid, i, j + 1, m, n);
+                  return dp[i][j] = grid[i][j] + solve(grid, i, j + 1, m, n, dp);
             } else if (j == n - 1) {
                   // last col --> only go down
-                  return grid[i][j] + solve(grid, i + 1, j, m, n);
+                  return dp[i][j] = grid[i][j] + solve(grid, i + 1, j, m, n, dp);
             } else {
-                  return grid[i][j] + Math.min(solve(grid, i + 1, j, m, n), solve(grid, i, j + 1, m, n));
+                  return dp[i][j] = grid[i][j] + Math.min(solve(grid, i + 1, j, m, n, dp), solve(grid, i, j + 1, m, n, dp));
             }
       }
 
       public static int minPathSum(int[][] grid) {
             int m = grid.length;
             int n = grid[0].length;
-            return solve(grid, 0, 0, m, n);
+
+            int dp[][] = new int[m+1][n+1];
+            for(int row[] : dp){
+                  Arrays.fill(row, -1);
+            }
+            return solve(grid, 0, 0, m, n, dp);
       }
 
       public static void main(String[] args) {
