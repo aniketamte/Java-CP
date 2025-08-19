a558de035c009f313a9ce9abe8cb18d958bf514d
aniketamte <aniketamte123@gmail.com>
2025-08-19 19:09:33 +0530

    Leetcode -> 2348

--- POTD/Leetcode2348.java
+++ POTD/Leetcode2348.java
@@ -1,24 +1,46 @@
+
 public class Leetcode2348 {
+    //Approach - 1
+
     public static long zeroFilledSubarray(int[] nums) {
-      long result = 0;
-      int i = 0;
-      int n = nums.length;
-      while(i<n){
+        long result = 0;
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
             long length = 0;
-            if(nums[i] == 0){
-                  while(i<n && nums[i]==0){
-                        i++;
-                        length++;
-                  }
-            } else{
-                  i++;
+            if (nums[i] == 0) {
+                while (i < n && nums[i] == 0) {
+                    i++;
+                    length++;
+                }
+            } else {
+                i++;
             }
-            result += (length)*(length+1)/2;
-      }
-      return result;
+            result += (length) * (length + 1) / 2;
+        }
+        return result;
     }
+
+    //Approach - 2
+    public static long zeroFilledSubarray2(int[] nums) {
+        long result = 0;
+        int n = nums.length;
+        long count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                count = 0;
+            }
+            result = result + count;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-      int nums[] = {1, 3, 0, 0, 2, 0, 0, 4};
-      System.out.println(zeroFilledSubarray(nums));
+        int nums[] = {1, 3, 0, 0, 2, 0, 0, 4};
+        System.out.println(zeroFilledSubarray(nums));
+        System.out.println(zeroFilledSubarray2(nums));
     }
 }
