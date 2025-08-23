40039b39dc0d20b9df0771fefc27b39fc10ab456
aniketamte <aniketamte123@gmail.com>
2025-08-23 11:26:01 +0530

    Leecode - 1074 ==> Number of Submatrices That Sum to Target

--- Arrays/Arrays-I/L1074.java
+++ Arrays/Arrays-I/L1074.java
@@ -1,5 +1,9 @@
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class L1074 {
+    //Brute Force
 
     public static int numSubmatrixSumTarget(int[][] matrix, int target) {
         int m = matrix.length;
@@ -27,6 +31,44 @@ public class L1074 {
         return result;
     }
 
+    public static int numSubmatrixSumTarget2(int[][] matrix, int target) {
+        int rows = matrix.length; // m
+        int cols = matrix[0].length; // n
+
+        // First take the cumulative sum row-wise
+        for (int row = 0; row < rows; row++) {
+            for (int col = 1; col < cols; col++) {
+                matrix[row][col] += matrix[row][col - 1];
+            }
+        }
+
+        // Now, find the "No. of subarrays with sum k" in the downward direction
+        int result = 0;
+        for (int startCol = 0; startCol < cols; startCol++) {
+            for (int currCol = startCol; currCol < cols; currCol++) {
+                // We need to find all submatrices sum
+
+                // Now comes the concept of "No. of subarrays with sum k"
+                Map<Integer, Integer> map = new HashMap<>();
+                map.put(0, 1);
+                int sum = 0;
+
+                // Go downwards row-wise
+                for (int row = 0; row < rows; row++) {
+                    sum += matrix[row][currCol] - (startCol > 0 ? matrix[row][startCol - 1] : 0);
+
+                    if (map.containsKey(sum - target)) {
+                        result += map.get(sum - target);
+                    }
+
+                    map.put(sum, map.getOrDefault(sum, 0) + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         int matrix[][] = {
             {0, 1, 0},
@@ -35,5 +77,6 @@ public class L1074 {
         };
         int target = 0;
         System.out.println(numSubmatrixSumTarget(matrix, target));
+        System.out.println(numSubmatrixSumTarget2(matrix, target));
     }
 }
