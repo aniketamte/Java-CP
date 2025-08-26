439abcb0af9cf06da574139c365792bf16d3f413
aniketamte <aniketamte123@gmail.com>
2025-08-26 15:52:07 +0530

    Leetcode - 583 ==> Minimum Insertions/Deletions to Convert String A to String B

---
+++ Dynamic Programming/A2Z/DP On String/Leetcode583.java
@@ -0,0 +1,50 @@
+public class Leetcode583 {
+
+    public static int longestCommonSubsequence2(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return dp[n][m];
+    }
+
+    public static int minDistance(String str1, String str2) {
+        return str1.length() + str2.length() - 2 * longestCommonSubsequence2(str1, str2);
+    }
+
+    public static void main(String[] args) {
+        String word1 = "sea";
+        String word2 = "eat";
+
+        int result = minDistance(word1, word2);
+        System.out.println("Minimum deletions to make \"" + word1 + "\" and \"" + word2 + "\" equal = " + result);
+    }
+}
