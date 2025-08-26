58286224f16e5ea8644ee6469993a48c4bab51b2
aniketamte <aniketamte123@gmail.com>
2025-08-26 15:30:00 +0530

    Leetcode - 512 ==> Minimum Insertion Steps to Make a String Palindrome (Memoization)

--- Dynamic Programming/A2Z/DP On String/Leetcode1312.java
+++ Dynamic Programming/A2Z/DP On String/Leetcode1312.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
 public class Leetcode1312 {
-      public static int solve(String s, int i, int j){
+      public static int solve(String s, int i, int j, int dp[][]){
             if(i >= j){
                   return 0;
             }
+
+            if(dp[i][j] != -1){
+                  return dp[i][j];
+            }
+            
             if(s.charAt(i) == s.charAt(j)){
-                  return solve(s, i+1, j-1);
+                  return solve(s, i+1, j-1, dp);
             } else{
-                  return 1 + Math.min(solve(s, i+1, j), solve(s, i, j-1));
+                  return 1 + Math.min(solve(s, i+1, j, dp), solve(s, i, j-1, dp));
             }
       }
 
       public static int minInsertions(String s){
             int n = s.length();
-            return solve(s, 0, n-1);
+            int dp[][] = new int[n][n];
+            for(int row[] : dp){
+                  Arrays.fill(row, -1);
+            }
+            return solve(s, 0, n-1, dp);
       }
 
       public static void main(String[] args) {
