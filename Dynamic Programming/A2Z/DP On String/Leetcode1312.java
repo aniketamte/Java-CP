ca7f0fb53ac8c69a8db9b16023ba8fef48f8e402
aniketamte <aniketamte123@gmail.com>
2025-08-26 15:34:59 +0530

    Leetcode - 512 ==> Minimum Insertion Steps to Make a String Palindrome (Bottom Up)

--- Dynamic Programming/A2Z/DP On String/Leetcode1312.java
+++ Dynamic Programming/A2Z/DP On String/Leetcode1312.java
@@ -8,7 +8,7 @@ public class Leetcode1312 {
             if(dp[i][j] != -1){
                   return dp[i][j];
             }
-            
+
             if(s.charAt(i) == s.charAt(j)){
                   return solve(s, i+1, j-1, dp);
             } else{
@@ -25,8 +25,27 @@ public class Leetcode1312 {
             return solve(s, 0, n-1, dp);
       }
 
+      public static int minInsertions2(String s){
+            int n = s.length();
+            int dp[][] = new int[n][n];
+
+            for (int L = 2; L <= n; L++) {
+              for (int i = 0; i < n - L + 1; i++) {
+                  int j = i + L - 1;
+                  if (s.charAt(i) == s.charAt(j)) {
+                      dp[i][j] = dp[i + 1][j - 1];
+                  } else {
+                      dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                  }
+              }
+          }
+
+          return dp[0][n - 1];
+      }
+
       public static void main(String[] args) {
             String s = "leetcode";
+            System.out.println(minInsertions(s));
             System.out.println(minInsertions(s));
       }
 }
