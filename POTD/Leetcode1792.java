da7f86500d776423af0125363fb06b4bb8c21af3
aniketamte <aniketamte123@gmail.com>
2025-09-01 12:30:44 +0530

    Leetcode - 1792 ==> Maximum Average Pass Ratio

--- POTD/Leetcode1792.java
+++ POTD/Leetcode1792.java
@@ -1,4 +1,4 @@
-
+import java.util.PriorityQueue;
 
 public class Leetcode1792 {
 
@@ -37,11 +37,40 @@ public class Leetcode1792 {
         }
         return result/n;
     }
+
+    public static double maxAverageRatio2(int[][] classes, int extraStudents) {
+        int n = classes.length;
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+        for (int i = 0; i < n; i++) {
+            double currentPR = (double) classes[i][0] / classes[i][1];
+            double newPR = (double) (classes[i][0] + 1) / (classes[i][1] + 1);
+            double delta = newPR - currentPR;
+            pq.offer(new double[]{delta, i});
+        }
+        while (extraStudents-- > 0) {
+            double[] curr = pq.poll();
+            int idx = (int) curr[1];
+            classes[idx][0]++;
+            classes[idx][1]++;
+            double currentPR = (double) classes[idx][0] / classes[idx][1];
+            double newPR = (double) (classes[idx][0] + 1) / (classes[idx][1] + 1);
+            double delta = newPR - currentPR;
+            pq.offer(new double[]{delta, idx});
+        }
+        double result = 0.0;
+        for (int i = 0; i < n; i++) {
+            result += (double) classes[i][0] / classes[i][1];
+        }
+
+        return result / n;
+    }
+
     public static void main(String[] args) {
       int classes[][] = {
             {1,2}, {3,5}, {2,2}
       };
       int extraStudents = 2;
       System.out.println(maxAverageRatio(classes, extraStudents));
+      System.out.println(maxAverageRatio2(classes, extraStudents));
     }
 }
