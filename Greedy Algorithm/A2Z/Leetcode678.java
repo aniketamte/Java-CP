46b0b55a4957612dd0a02a8ee62bbb80f0639d71
aniketamte <aniketamte123@gmail.com>
2025-09-04 17:45:49 +0530

    Leetcode - 678 ==> Valid Parenthesis String (Bottom -Up)

--- Greedy Algorithm/A2Z/Leetcode678.java
+++ Greedy Algorithm/A2Z/Leetcode678.java
@@ -1,4 +1,6 @@
+
 public class Leetcode678 {
+
     public static boolean checkValidString(String s) {
         int n = s.length();
         Boolean[][] dp = new Boolean[n + 1][n + 1]; // memo table
@@ -38,8 +40,41 @@ public class Leetcode678 {
         return dp[idx][open] = isValid;
     }
 
+    public static boolean checkValidString2(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n + 1][n + 1];
+
+        dp[n][0] = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int open = 0; open <= n; open++) {
+                boolean isValid = false;
+                char c = s.charAt(i);
+                if (c == '*') {
+                    if (open + 1 <= n) {
+                        isValid |= dp[i + 1][open + 1];
+                    }
+                    isValid |= dp[i + 1][open];
+
+                    if (open > 0) {
+                        isValid |= dp[i + 1][open - 1];
+                    }
+                } else if (c == '(') {
+                    if (open + 1 <= n) {
+                        isValid |= dp[i + 1][open + 1];
+                    }
+                } else if (open > 0) {
+                    isValid |= dp[i + 1][open - 1];
+                }
+                dp[i][open] = isValid;
+            }
+        }
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
         String s = "(*))";
         System.out.println(checkValidString(s)); // true
+        System.out.println(checkValidString2(s));
     }
 }
