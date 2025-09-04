8faa6e3a198ec0dba844521db898c60d684d2802
aniketamte <aniketamte123@gmail.com>
2025-09-04 12:28:51 +0530

    Activity Selection Using Greedy Algorithm

--- Greedy Algorithm/Basic/ActivitySelectionCode.java
+++ Greedy Algorithm/Basic/ActivitySelectionCode.java
@@ -1,6 +1,4 @@
-
-import java.util.ArrayList;
-
+import  java.util.*;
 public class ActivitySelectionCode {
 
     public static int activitySelection(int start[], int end[]) {
@@ -30,6 +28,15 @@ public class ActivitySelectionCode {
     public static void main(String[] args) {
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[] = {2, 4, 6, 7, 9, 9};
+
+        //Sorting
+        int activities[][] = new int[start.length][3];
+        for(int i=0; i<start.length; i++){
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
         System.out.println(activitySelection(start, end));
     }
 }
