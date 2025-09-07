1b2975939cc40615b721f47aca26e17dc605bed3
aniketamte <aniketamte123@gmail.com>
2025-09-08 00:31:03 +0530

    Practice

---
+++ Recursion/Practice/Sorted.java
@@ -0,0 +1,13 @@
+public class Sorted {
+    public static boolean sorted(int arr[], int index) {
+        if (index == arr.length - 1) {
+            return true;
+        }
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 6, 4, 5};
+        System.out.println(sorted(arr, 0));
+    }
+}
