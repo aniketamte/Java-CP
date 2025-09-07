3de5f5d8215551ddd34b6517590b675eb17e6dff
aniketamte <aniketamte123@gmail.com>
2025-09-07 15:51:58 +0530

    Leetcode - 1304 ==> Find N Unique Integers Sum up to Zero

--- POTD/Leetcode1304.java
+++ POTD/Leetcode1304.java
@@ -16,8 +16,25 @@ public class Leetcode1304 {
         return res;
     }
 
+    //Approach 2
+    public static int[] sumZero2(int n) {
+        int res[] = new int[n];
+        int start = 1;
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            res[i] = start;
+            res[j] = -start;
+            i++;
+            j--;
+            start++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println(Arrays.toString(sumZero(n)));
+        System.out.println(Arrays.toString(sumZero2(n)));
     }
 }
