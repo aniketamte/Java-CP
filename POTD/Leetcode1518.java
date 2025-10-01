0e3147ee54363aaaa6e0a73a642e46980ecc5bca
aniketamte <aniketamte123@gmail.com>
2025-10-01 10:03:08 +0530

    Leetcode - 1518 ==> Water Bottles

--- POTD/Leetcode1518.java
+++ POTD/Leetcode1518.java
@@ -8,10 +8,26 @@ public class Leetcode1518 {
             }
             return consumed + numBottles;
       }
+
+      //Approach2
+
+      public static int numWaterBottles2(int numBottles, int numExchange){
+            int consumed = numBottles;
+            int emptyBottle = numBottles;
+
+            while(emptyBottle >= numExchange){
+                  int extraFullBottle = emptyBottle / numExchange;
+                  int remain = emptyBottle % numExchange;
+                  consumed += extraFullBottle;
+                  emptyBottle = remain + extraFullBottle;
+            }
+            return consumed;
+      }
       
       public static void main(String[] args) {
            int numBottles = 9;
            int numExchange = 3;
            System.out.println(numWaterBottles(numBottles, numExchange)); 
+           System.out.println(numWaterBottles2(numBottles, numExchange));
       }
 }
