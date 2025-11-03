4b64604e664475a2dbc025c4e936ffcbe9f952fc
aniketamte <aniketamte123@gmail.com>
2025-11-03 10:49:47 +0530

    Practice

---
+++ LinkedList/Assignment/validParen.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class validParen{
+      public static boolean isValid(String s){
+            Stack<Character> st = new Stack<>();
+            for(int i=0; i<s.length(); i++){
+                  char ch = s.charAt(i);
+                  if(ch == '(' || ch == '{' || ch == '['){ //opening
+                        st.push(ch);
+                  } else{
+                        //closing
+                        if(st.isEmpty()){
+                              return false;
+                        }
+                        if((st.peek() == '(' && ch == ')')
+                        || (st.peek() == '{' && ch == '}')
+                        || (st.peek() == '[' && ch == ']')){
+                              st.pop();
+                        } else{
+                              return false;
+                        }
+                  }
+            }
+            if(st.isEmpty()){
+                  return true;
+            } else{
+                  return false;
+            }
+      }
+
+      public static void main(String[] args) {
+            String s = "({})[]";
+            System.out.println(isValid(s));
+      }
+}
